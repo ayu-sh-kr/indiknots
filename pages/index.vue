215658acf74138aa983ac696dda820164dc3ffe1
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-05-28 14:02:23 +0530

    Update Index.vue: added component of Category to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 
 import HeroSection from "~/components/home/HeroSection.vue";
+import ShopCategory from "~/components/home/ShopCategory.vue";
 </script>
 
 <template>
     <HeroSection/>
+    <ShopCategory/>
 </template>
 
 <style scoped>
