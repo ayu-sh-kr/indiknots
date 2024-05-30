3c58e116707be285add7e851324fd3c5592a4db8
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-05-31 03:06:37 +0530

    Added Components to home page

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,15 @@
 
 import HeroSection from "~/components/home/HeroSection.vue";
 import ShopCategory from "~/components/home/ShopCategory.vue";
+import RugPurpose from "~/components/home/RugPurpose.vue";
+import BestSelling from "~/components/home/BestSelling.vue";
 </script>
 
 <template>
     <HeroSection/>
     <ShopCategory/>
+    <RugPurpose/>
+    <BestSelling/>
 </template>
 
 <style scoped>
