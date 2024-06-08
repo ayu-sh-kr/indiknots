f4aabf21ae187ce8faa953057d2efd297bb7862c
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-06-08 13:00:34 +0530

    Create SectionHeader.vue: Component to replace the future declaration of header in section

---
+++ components/utils/SectionHeader.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps({
+    text1: {
+        type: String,
+        required: true
+    },
+
+    text2: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="flex justify-center text-center items-center">
+        <h3>{{ text1 }} <span class="text-orange-400">{{ text2 }}</span></h3>
+    </div>
+</template>
+<style scoped>
+
+</style>
