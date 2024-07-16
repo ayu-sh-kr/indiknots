d35a39fc22ee6f6155f8203bdd801d61fc5347f5
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-07-16 22:32:28 +0530

    Update app.vue: Added Seo Meta Data

--- app.vue
+++ app.vue
@@ -6,4 +6,13 @@
   </div>
 </template>
 <script setup lang="ts">
+    useSeoMeta({
+        title: 'Indiknots: Carpet and Rugs Seller',
+        ogTitle: 'Indiknots: Carpet Manufacturer and Exporter in Bhadohi, Uttar Pradesh, India',
+        description: `
+            Indiknots is a leading manufacturer of handmade, classical and traditional rugs and carpet. It is a Bhadohi, India based company shipping products globally.
+            At Indiknots we provide buyer with ready to ship, custom design and bulk order facilities on the go. We always keep up with the market trends and provide our customers
+            with best in quality products. Our rugs are manufactures with the local craftsmanship which is developed over centuries and our workers show excellence in their skills.
+        `
+    })
 </script>
