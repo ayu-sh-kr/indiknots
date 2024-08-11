37088f9e38608f4bfad4867141c53df764f36f88
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-12 00:11:22 +0530

    Update NoContent.vue: UI changes

--- components/utils/NoContent.vue
+++ components/utils/NoContent.vue
@@ -4,8 +4,11 @@ import NoData from '~/assets/svg/no-data.svg';
 
 <template>
 <section class="grid sm:grid-cols-1 md:grid-cols-2 max-w-7xl mx-auto place-items-center overflow-hidden">
-    <h1 class="text-orange-400 py-10">No Content At The Moment</h1>
-    <NoData style="height: 100%; width: 100%"/>
+    <div class="text-center py-10 space-y-5 font-semibold text-gray-700 dark:text-gray-300">
+        <h4 class="text-5xl md:text-6xl lg:text-7xl">Oops!</h4>
+        <h1 class="text-orange-400">No Content At The Moment</h1>
+    </div>
+    <NoData style="height: 100%; width: 100%" :title="'No-Data svg by freepik'"/>
 </section>
 </template>
 
