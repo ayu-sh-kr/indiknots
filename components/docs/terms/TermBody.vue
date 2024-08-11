961c31943bd7868775e43b17c0064c7a08beae81
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-11 10:27:14 +0530

    Create TermBody.vue: Component to render conditions of T&C

---
+++ components/docs/terms/TermBody.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+import DOMPurify from 'dompurify';
+
+const props = defineProps({
+    texts: {
+        type: Array,
+        required: true
+    }
+});
+
+
+const paras = ref<string[]>(<string[]>props.texts);
+
+</script>
+
+<template>
+
+    <div class="grid space-y-4 mt-3 text-lg">
+        <p v-for="para in paras" v-html="para" :key="para"/>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
