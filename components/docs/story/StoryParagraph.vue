cfdef17a500e8dbcbc5a0e2639515eeab4be08f4
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-11 18:58:30 +0530

    Create StoryParagraph.vue: paragraph tags with custom styling

---
+++ components/docs/story/StoryParagraph.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    text: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+
+    <p class="text-base sm:text-lg text-gray-800 dark:text-gray-200" v-html="text"/>
+
+</template>
+
+<style scoped>
+
+</style>
