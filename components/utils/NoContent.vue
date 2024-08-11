46b3d23091e33f3c476904205775cdd3042f31f7
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-12 00:05:03 +0530

    Create NoContent.vue: Component to show when no content available

---
+++ components/utils/NoContent.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import NoData from '~/assets/svg/no-data.svg';
+</script>
+
+<template>
+<section class="grid sm:grid-cols-1 md:grid-cols-2 max-w-7xl mx-auto place-items-center overflow-hidden">
+    <h1 class="text-orange-400 py-10">No Content At The Moment</h1>
+    <NoData style="height: 100%; width: 100%"/>
+</section>
+</template>
+
+<style scoped>
+
+</style>
