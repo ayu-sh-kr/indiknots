018f11fb0a78a515ea207751c425848894706544
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-11 10:28:29 +0530

    Update OurStory.vue

--- pages/docs/OurStory.vue
+++ pages/docs/OurStory.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 
+import SectionHeader from "~/components/utils/SectionHeader.vue";
 </script>
 
 <template>
-
+<section class="py-6 md:py-16 px-2 min-h-screen font-dm">
+    <SectionHeader text2="STORY" text1="OUR" />
+</section>
 </template>
 
 <style scoped>
