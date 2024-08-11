56507ed6e1d0accd52a5b037a7b9bb3d57df543d
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-11 16:07:52 +0530

    Update OurStory.vue

--- pages/docs/OurStory.vue
+++ pages/docs/OurStory.vue
@@ -4,8 +4,12 @@ import SectionHeader from "~/components/utils/SectionHeader.vue";
 </script>
 
 <template>
-<section class="py-6 md:py-16 px-2 min-h-screen font-dm">
-    <SectionHeader text2="STORY" text1="OUR" />
+<section class="py-6 md:py-16 px-2 h-full font-dm">
+    <SectionHeader text2="FOUNDER" text1="THE" />
+
+    <div class="grid max-w-7xl mx-auto">
+        
+    </div>
 </section>
 </template>
 
