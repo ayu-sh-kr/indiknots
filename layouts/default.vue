0afa6a392fc73511550a092fd079b2ad8aff5a78
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-11 10:28:11 +0530

    Update defualt.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,7 @@ console.log(isSideNavActive.value)
 
 </script>
 
-<template class="hide-scrollbar">
+<template class="hide-scrollbar h-screen grid grid-rows-[1fr_auto] font-dm">
     <AppHeader @side-nav="toggleSidenav"/>
     <slot/>
     <AppFooter/>
