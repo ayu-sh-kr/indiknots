e2a9b6d5b001c043a3c47d68156ea2482d8bd16b
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-11 10:26:32 +0530

    Create TermHeader.vue Component to render header of T&C

---
+++ components/docs/terms/TermHeader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    text: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <h4 class="font-medium text-xl text-gray-900">{{text}}</h4>
+</template>
+
+<style scoped>
+
+</style>
