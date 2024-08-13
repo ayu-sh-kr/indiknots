c713c6ea4c7946822bba0a1789c032c59c510fec
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-13 18:36:07 +0530

    Update SideNav.vue: Removed Vertical Navbar and added pop over for navigation on small screen size

--- components/utils/SideNav.vue
+++ components/utils/SideNav.vue
@@ -17,23 +17,29 @@ export interface Link {
     iconClass?: string
 }
 
-export interface IconType {
-    icon: string,
-    color?: string
-}
-
 </script>
 
 <template>
-    <UVerticalNavigation :links="links"
-                         :ui="{
-                               icon: {
-                                   active: 'text-orange-400 dark:text-orange-600',
-                                   inactive: 'text-gray-500 dark:text-gray-200 group-hover:text-gray-800 dark:group-hover:text-gray-50'
-                               }
+    <UPopover>
+        <button class="lg:hidden focus:outline-none cursor-pointer focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0
+                            font-medium rounded-md text-sm gap-x-1.5 p-1.5 text-gray-700 dark:text-orange-400 hover:text-gray-900 dark:hover:text-orange-600 hover:bg-gray-100
+                            dark:hover:bg-gray-900 focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center"
+        >
+            <UIcon name="i-mdi-menu" dynamic class="flex-shrink-0 h-5 w-5 font-semibold"/>
+        </button>
 
-                         }"
-    />
+        <template #panel>
+            <div class="flex flex-col w-56 gap-x-1.5 py-4">
+                <NuxtLink v-for="link in links" :to="link.to"
+                          class="transition-all py-2 px-3 flex justify-start gap-x-3 items-center cursor-pointer dark:hover:bg-gray-800
+                                hover:bg-gray-50 text-slate-900 hover:text-orange-400 dark:text-gray-100 dark:hover:text-orange-400"
+                >
+                    <UIcon :name="link.icon || 'Unavailable'"/>
+                    <h4>{{link.label}}</h4>
+                </NuxtLink>
+            </div>
+        </template>
+    </UPopover>
 </template>
 
 <style scoped>
