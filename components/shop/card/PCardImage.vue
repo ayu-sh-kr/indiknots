d4569476d3cca0e0dffac2658513d38866ba5cc0
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-30 23:49:37 +0530

    Update PCardImage.vue
    Update style and added tag content actions

--- components/shop/card/PCardImage.vue
+++ components/shop/card/PCardImage.vue
@@ -1,17 +1,39 @@
 <script setup lang="ts">
 
+import TagButton from "~/components/shop/card/TagButton.vue";
+
 const props = defineProps({
-    url: String
-})
+    url: String,
+    product: {
+        type: Object as PropType<Product>,
+        required: false
+    }
+});
 
 </script>
 
 <template>
-    <div class="w-[280px] h-96 overflow-hidden bg-transparent">
+    <div class="p-image relative w-[280px] h-96 overflow-hidden bg-transparent">
+        <div class="tag h-full absolute right-0 flex flex-col gap-y-5 items-center p-2">
+            <TagButton icon="material-symbols-light:shopping-cart-rounded" title="Add to cart"/>
+            <TagButton icon="material-symbols-light:favorite" title="Add to favourite"/>
+        </div>
         <img :src="url" alt="Rug Image" class="w-full h-full">
     </div>
 </template>
 
 <style scoped>
+
+.tag {
+    transition-duration: .4s;
+    opacity: 0;
+    transform: translateX(100%);
+}
+
+.p-image:hover > .tag {
+    transition-duration: .4s;
+    opacity: 1;
+    transform: translateX(0);
+}
 
 </style>
