2f51e466804917b7f20c8122e15ad3983ef930e6
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-30 22:09:44 +0530

    Create PCardImage.vue
    Pre-styled component for Product Image

---
+++ components/shop/card/PCardImage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    url: String
+})
+
+</script>
+
+<template>
+    <div class="w-[280px] h-96 overflow-hidden bg-transparent">
+        <img :src="url" alt="Rug Image" class="w-full h-full">
+    </div>
+</template>
+
+<style scoped>
+
+</style>
