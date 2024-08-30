1d48c377c5c3e041259e195d9ab7d5d4320a6efe
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-31 00:39:30 +0530

    Update index.vue
    On Shop page added pagination

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -12,12 +12,18 @@ useHead({
 const products = ref<Product[]>();
 const cart: Map<number, string> = new Map();
 
+const page = ref(1)
+
 onMounted(async () => {
     const response = await fetch('/data/product.json');
 
     if(response.status === 200) {
         products.value = await response.json() as Product[]
     }
+})
+
+const visible = computed(() => {
+    return products.value?.slice((page.value -1) * 10, (page.value) * 10);
 })
 
 </script>
@@ -27,8 +33,12 @@ onMounted(async () => {
         <SectionHeader text2="Products" text1="Shop Our" />
 
 
-        <div class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 max-sm:place-items-center mt-10">
-            <Product v-for="product in products" :key="product.id" :product="product" :cart="cart"/>
+        <div class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 max-sm:place-items-center mt-10 gap-y-20 gap-x-10">
+            <Product v-for="product in visible" :key="product.id" :product="product" :cart="cart"/>
+        </div>
+
+        <div class="flex justify-center items-center mt-10 border-t pt-5">
+            <UPagination v-model="page" :page-count="10" :total="products.length" show-first show-last/>
         </div>
 
 
