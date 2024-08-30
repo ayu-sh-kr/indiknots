ebd3c5fefa1f201d6d33b881ff21a6b96b2ea791
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-30 22:08:14 +0530

    Create Scaffold.vue
    Component to provide pre-styled sections

---
+++ components/utils/Scaffold.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<section class="py-10 px-2 sm:font-dm font-tahoma relative">
+    <slot/>
+</section>
+</template>
+
+<style scoped>
+
+</style>
