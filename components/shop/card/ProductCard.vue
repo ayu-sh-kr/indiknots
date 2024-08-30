9582ba22bb42a374f89006cf17c8f62ea1d78699
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-30 22:09:14 +0530

    Create ProductCard.vue
    Component to wrap card with pre-styled height and width

---
+++ components/shop/card/ProductCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center justify-center space-y-3">
+        <slot/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
