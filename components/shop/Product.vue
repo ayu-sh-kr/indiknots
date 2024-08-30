dab0a611d6cfb7b94695888f11da7a1424186f70
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-31 00:18:28 +0530

    Update Product.vue
    Listen to custom event add-cart, implemented a Temporary function to mimic add to cart and removed from cart

--- components/shop/Product.vue
+++ components/shop/Product.vue
@@ -7,8 +7,14 @@ const props = defineProps({
     product: {
         type: Object as PropType<Product>,
         required: true
+    },
+    cart: {
+        type: Map,
+        required: true
     }
 });
+
+const toast = useToast();
 
 const capitalize = (str: string): string => {
     if (!str) return str;
@@ -28,13 +34,38 @@ const processProductLength = (size: ProductSize) => {
     return `${size.length} x ${size.width} ${size.unit}`
 }
 
+const addToCart = () => {
+
+    let product = props.product;
+    let cart = props.cart;
+
+    if(cart.get(product.id)) {
+        toast.add({
+            color: "orange",
+            title: "Removed",
+            description: `${props.product.name} removed from cart`,
+            timeout: 5000,
+            icon: "i-heroicons-check-circle"
+        })
+    } else {
+        cart.set(product.id, product.name)
+        toast.add({
+            color: "orange",
+            title: "Added",
+            description: `${props.product.name} added to cart`,
+            timeout: 5000,
+            icon: "i-heroicons-check-circle"
+        })
+    }
+}
+
 </script>
 
 <template>
 <ProductCard>
-    <PCardImage :url="product.img"/>
+    <PCardImage @addCart="addToCart" :url="product.img"/>
     <div class="text-center">
-        <h4 class="text-xl font-medium text-gray-800 dark:text-gray-100">{{ product.name.toUpperCase() }}</h4>
+        <h4 class="text-lg font-medium text-gray-800 dark:text-gray-100">{{ product.name.toUpperCase() }}</h4>
         <p class="font-extralight text-gray-600 dark:text-gray-300 text-sm">{{processShortDescription(product)}}</p>
         <p class="font-light text-gray-800/95 dark:text-gray-200/95 mt-2">{{processProductLength(product.size[0])}}</p>
         <p class="text-gray-950 dark:text-gray-50 text-lg">From ${{product.price[0].price}}</p>
