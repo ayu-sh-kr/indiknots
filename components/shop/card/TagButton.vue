f69923e20dd3035d61c94de569d2065ff01d5fe2
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-30 23:48:34 +0530

    Create  TagButton.vue
    Custom Icon encapusulated button for certain actions

---
+++ components/shop/card/TagButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    icon: {
+        type: String,
+        required: true
+    }
+})
+
+</script>
+
+<template>
+<div class="p-2 rounded-full bg-gray-50 dark:bg-gray-800 cursor-pointer">
+    <UIcon :name="`i-${icon}`" class="text-2xl text-gray-900/80 dark:text-slate-200 hover:text-orange-400 dark:hover:text-orange-500 "/>
+</div>
+</template>
+
+<style scoped>
+
+</style>
