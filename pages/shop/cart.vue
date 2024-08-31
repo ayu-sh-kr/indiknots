8c86e3312796ed8d6cf533802833725feef9dc09
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-08-31 23:28:15 +0530

    Create cart.vue
    Cart page for the app

---
+++ pages/shop/cart.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+import Scaffold from "~/components/utils/Scaffold.vue";
+import SectionHeader from "~/components/utils/SectionHeader.vue";
+</script>
+
+<template>
+    <Scaffold class="min-h-screen">
+        <SectionHeader text2="Cart" text1=""/>
+
+
+
+    </Scaffold>
+</template>
+
+<style scoped>
+
+</style>
