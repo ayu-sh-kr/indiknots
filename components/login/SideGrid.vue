9db5e5cfecd10da78f1634681f6165f8b4993f5b
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-01 01:56:52 +0530

    Create SideGrid.vue
    Template for login page -> will make it re-usable in future

---
+++ components/login/SideGrid.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-col items-center bg-orange-300 rounded-xl max-sm:h-16 max-md:h-32 overflow-hidden">
+        <div>
+            <img src="/images/login/login.png" alt="Login Rug">
+        </div>
+
+        <h2 class="text-white font-semibold">Be Trendy</h2>
+
+        <p class="my-5 text-white text-sm font-light w-96 text-center text-wrap">Buy the best rugs, made by generational experienced weavers</p>
+
+    </div>
+</template>
