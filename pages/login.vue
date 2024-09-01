434a9c38b30d2c085fc42127937311f0c4c63a24
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-01 21:09:43 +0530

    Update login.vue
    Fix Responsivness

--- pages/login.vue
+++ pages/login.vue
@@ -18,9 +18,9 @@ isDark.value = false;
 </script>
 
 <template>
-    <Scaffold class="grid place-items-center h-screen bg-slate-200 w-full">
+    <Scaffold class="grid bg-slate-200 h-screen w-full place-items-center">
 
-        <div class="grid md:grid-cols-2 grid-cols-1 bg-white shadow-2xl rounded-2xl p-2 sm:p-4 md:p-6 lg:p-8 border border-gray-100 gap-5 min-w-[340px] sm:min-w-[580px] md:min-w-[720px] lg:w-[1080px]">
+        <div class="grid md:grid-cols-2 grid-cols-1 bg-white shadow-2xl rounded-2xl p-2 sm:p-4 md:p-6 lg:p-8 border border-gray-100 gap-5 max-w-[340px] sm:max-w-[580px] md:max-w-[720px] lg:max-w-[920px] h-fit">
 
             <SideGrid/>
 
