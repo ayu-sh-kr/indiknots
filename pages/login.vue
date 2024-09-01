363e8e13ebe18b6a3d1c909bac0a2a7d60d8b044
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-01 21:18:21 +0530

    Update login.vue -> Removed width optimization from the grid

--- pages/login.vue
+++ pages/login.vue
@@ -20,7 +20,7 @@ isDark.value = false;
 <template>
     <Scaffold class="grid bg-slate-200 h-screen w-full place-items-center">
 
-        <div class="grid md:grid-cols-2 grid-cols-1 bg-white shadow-2xl rounded-2xl p-2 sm:p-4 md:p-6 lg:p-8 border border-gray-100 gap-5 max-w-[340px] sm:max-w-[580px] md:max-w-[720px] lg:max-w-[920px] h-fit">
+        <div class="grid md:grid-cols-2 grid-cols-1 bg-white shadow-2xl rounded-2xl p-2 sm:p-4 md:p-6 lg:p-8 border border-gray-100 gap-5 w-fit h-fit">
 
             <SideGrid/>
 
