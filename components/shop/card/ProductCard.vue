9244ee31ee89cc2ffda67873d671d63886fb4553
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-03 15:25:14 +0530

    Update ProductCard.vue
    Scale down the card size to change the appearance

--- components/shop/card/ProductCard.vue
+++ components/shop/card/ProductCard.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <div class="relative flex flex-col items-center justify-center gap-y-3 w-fit overflow-hidden">
+    <div class="relative flex flex-col items-center justify-center gap-y-3 w-fit overflow-hidden scale-90">
         <slot/>
     </div>
 </template>
