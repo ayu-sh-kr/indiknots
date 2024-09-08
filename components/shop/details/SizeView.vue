9fa6e9333a9319928746b7efcda06932ae94d06e
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-09 00:21:01 +0530

    Update SizeView.vue
    Added function to process info for rounded and non rounded rug

--- components/shop/details/SizeView.vue
+++ components/shop/details/SizeView.vue
@@ -10,7 +10,16 @@ const props = defineProps({
         type: Boolean,
         required: true
     }
-})
+});
+
+const processProductLength = (size: ProductSize) => {
+
+    if(size.width === 0) {
+        return `Rounded ${size.length} ${size.unit}`
+    }
+
+    return `${size.length} x ${size.width} ${size.unit}`
+}
 
 </script>
 
@@ -18,7 +27,7 @@ const props = defineProps({
     <div  class="px-2 py-1 text-center cursor-pointer"
           :class="`${selected ? 'bg-orange-400 dark:bg-orange-500 text-gray-50': 'border border-gray-600 dark:border-gray-200 text-gray-800  dark:text-gray-200'}`"
     >
-        {{size.length}} x {{size.width}} {{size.unit}}
+        {{processProductLength(size)}}
     </div>
 </template>
 
