8e88bd2b15eea42537ef50cb7a088e6018a5631d
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-08 22:13:06 +0530

    create product.vue
    page to show product details

---
+++ pages/shop/product.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+import Scaffold from "~/components/utils/Scaffold.vue";
+import SectionHeader from "~/components/utils/SectionHeader.vue";
+import ProductDetails from "~/components/shop/ProductDetails.vue";
+
+
+const route = useRoute();
+const productId = route.query.id as string
+
+useHead({
+    title: 'Indiknots | Product'
+});
+
+const product = ref<Product | null>()
+
+onMounted(() => {
+    product.value = history.state ? JSON.parse(history.state['product']) as Product : null;
+})
+</script>
+
+<template>
+    <Scaffold>
+        <SectionHeader text2="Details" text1="Product"/>
+        <ProductDetails v-if="product" :product="product"/>
+    </Scaffold>
+</template>
+
+<style scoped>
+
+</style>
