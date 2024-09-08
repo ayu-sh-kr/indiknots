265d950860e72cf5a3c141f4f92ff4ebeb9ea023
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-08 22:05:41 +0530

    Create ViewButton.vue
    Animated Button Component for Product card

---
+++ components/shop/card/ViewButton.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <button class="relative group px-3 py-1 flex items-center transition-all active:scale-95 rounded-md overflow-hidden">
+        <span class="absolute -z-10 left-0 w-0 h-full group-hover:w-full dark:bg-orange-500 bg-orange-400 transition-all duration-300"></span>
+        <slot/>
+        <span class="absolute -z-10 right-0 w-0 h-full group-hover:w-full dark:bg-orange-500 bg-orange-400 transition-all duration-300"></span>
+    </button>
+</template>
+
+<style scoped>
+
+.background {
+    @apply relative -z-10 w-full h-full;
+}
+
+.background::before,
+.background::after {
+    @apply absolute dark:bg-orange-400 bg-orange-500 p-2;
+    content: '';
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    transition: width 1s ease;
+    left: 50%;
+    top: 50%;
+}
+
+button:hover > .background::before,
+button:hover > .background::after {
+    @apply px-20 py-5;
+    width: 60%;
+}
+
+
+</style>
