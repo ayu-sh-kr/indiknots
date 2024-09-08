e12e894fa816447d75e3732848a79702c16e1466
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-09 00:07:45 +0530

    Update SizeView.vue
    Update the UI -> Size for large screen is reduced and it is grid-col-3 now

--- components/shop/details/SizeView.vue
+++ components/shop/details/SizeView.vue
@@ -15,7 +15,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div  class="px-3 py-2 text-center cursor-pointer"
+    <div  class="px-2 py-1 text-center cursor-pointer"
           :class="`${selected ? 'bg-orange-400 dark:bg-orange-500 text-gray-50': 'border border-gray-600 dark:border-gray-200 text-gray-800  dark:text-gray-200'}`"
     >
         {{size.length}} x {{size.width}} {{size.unit}}
