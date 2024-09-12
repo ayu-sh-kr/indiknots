3cfad2fe178ed0d7d0de94d2fff62d6ba7218832
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-13 00:36:36 +0530

    Create MetaAction.vue
    MetaAction is used on the product page to provide action for sorting, filtering and refresh

---
+++ components/shop/details/MetaAction.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    icon: {
+        type: String,
+        required: false
+    },
+
+    text: {
+        type: String,
+        required: true
+    },
+
+    action: {
+        type: Boolean,
+        required: true
+    }
+});
+
+const StyleConfig = {
+    action: 'active:scale-95 cursor-pointer hover:bg-orange-100 dark:hover:bg-orange-800/20 hover:text-orange-400 dark:hover:text-orange-500'
+}
+
+</script>
+
+<template>
+    <p class="flex items-center gap-x-2 px-2 py-1 rounded-md transition-all duration-300"
+       :class="`${action && `${StyleConfig.action}`}`"
+    >
+        {{text}} <UIcon v-if="icon" :name="`i-${icon}`" class="text-xl"/>
+    </p>
+</template>
+
+<style scoped>
+
+</style>
