cac26a3a72609d55836dfb34ab297a989129b952
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-13 22:03:04 +0530

    Create HeaderCartAction.vue
    Encapsulated the code for cart icon on header component

---
+++ components/shop/cart/header/HeaderCartAction.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import {useCartStore} from "~/stores/cart.store";
+import CartPreview from "~/components/shop/cart/header/CartPreview.vue";
+import type {ProductModal} from "~/modals/product.modal";
+
+const cartStore = useCartStore();
+
+const products = ref<ProductModal[]>([])
+
+onMounted(() => {
+    products.value = cartStore.cart;
+})
+
+watch(() => cartStore.cart, (newProducts) => {
+    products.value = newProducts
+});
+
+</script>
+
+<template>
+    <UPopover mode="hover">
+        <UChip v-if="cartStore.getCartSize() > 0" :text="cartStore.getCartSize()" size="2xl">
+            <UButton to="/shop/cart" icon="i-material-symbols-light:shopping-cart-rounded" variant="ghost"/>
+        </UChip>
+
+        <UButton v-else to="/shop/cart" icon="i-material-symbols-light:shopping-cart-rounded" variant="ghost"/>
+
+        <template #panel>
+            <div v-for="(product, index) in products" :key="index">
+                <CartPreview :product="product" />
+            </div>
+        </template>
+    </UPopover>
+</template>
+
+<style scoped>
+
+</style>
