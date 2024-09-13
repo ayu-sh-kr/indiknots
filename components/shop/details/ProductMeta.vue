b028487821e5eff2b8617b85b41966a1f12f270b
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-13 22:04:22 +0530

    Update ProductMeta.vue
    change prop type for product to Product Modal
    remove some method and create class for repetitive tailwind

--- components/shop/details/ProductMeta.vue
+++ components/shop/details/ProductMeta.vue
@@ -3,10 +3,11 @@
 import ContentWrapper from "~/components/shop/details/ContentWrapper.vue";
 import SizeView from "~/components/shop/details/SizeView.vue";
 import {useCartStore} from "~/stores/cart.store";
+import type {ProductModal} from "~/modals/product.modal";
 
 const props = defineProps({
     product: {
-        type: Object as PropType<Product>,
+        type: Object as PropType<ProductModal>,
         required: true
     }
 });
@@ -18,13 +19,11 @@ onMounted(() => {
 });
 
 const calculatePrice = () => {
-    const prices = props.product.price.filter(price => price.size === selected.value);
 
-    if(prices[0]) {
-        console.log(prices[0]);
+    if(selected.value) {
+        const price = props.product.getPrizeBySize(selected.value)
+        return price ? `USD. ${price.price}` : 'Price not found';
     }
-
-    return prices[0] ? `USD. ${prices[0].price}` : 'Price not found';
 }
 
 const cartStore = useCartStore();
@@ -37,11 +36,15 @@ const cartActionButton = () => {
     }
 }
 
+const addedToCart = ref<boolean>(false)
+
 const addToCart = () => {
     if(!cartStore.isProductExist(props.product.id)) {
         cartStore.addToCart(props.product)
+        addedToCart.value = true;
     } else {
         cartStore.removeFromCart(props.product.id)
+        addedToCart.value = false;
     }
 }
 
@@ -55,9 +58,9 @@ const addToCart = () => {
         </ContentWrapper>
 
         <ContentWrapper class="flex gap-x-3 sm:gap-x-5 gap-y-3 flex-wrap">
-            <UBadge variant="soft">{{product.material.split("_").join(" ")}}</UBadge>
-            <UBadge variant="soft">{{product.category}}</UBadge>
-            <UBadge variant="soft">{{product.technique.split("_").join(" ")}}</UBadge>
+            <UBadge variant="soft">{{ product.getMaterialText() }}</UBadge>
+            <UBadge variant="soft">{{product.getCategoryText()}}</UBadge>
+            <UBadge variant="soft">{{product.getTechniqueText()}}</UBadge>
         </ContentWrapper>
 
         <ContentWrapper>
@@ -75,12 +78,12 @@ const addToCart = () => {
         </ContentWrapper>
 
         <div class="max-md:space-y-2 flex-col md:grid grid-cols-2 md:place-items-center items-center mt-5 w-full md:w-2/3 gap-x-5 transition-all">
-            <button class="w-full py-2 px-3 text-center transition-all bg-yellow-400 hover:bg-yellow-500 dark:bg-yellow-500 dark:hover:bg-yellow-600 text-white rounded-lg shadow-lg z-10 flex gap-x-3 justify-center items-center">
+            <button class="meta-action bg-yellow-400 hover:bg-yellow-500 dark:bg-yellow-500 dark:hover:bg-yellow-600">
                 <UIcon name="i-icon-park-outline:buy" class="text-lg font-semibold"/>
                 <span>Buy Now</span>
             </button>
-            <button @click="addToCart" class="w-full py-2 px-3 text-center transition-all bg-orange-400 hover:bg-orange-500 dark:bg-orange-500 dark:hover:bg-orange-600 text-white rounded-lg shadow-lg z-10 flex gap-x-3 justify-center items-center">
-                <UIcon name="i-material-symbols:add-shopping-cart-outline-rounded" class="text-lg font-semibold"/>
+            <button @click="addToCart" class="meta-action bg-orange-400 hover:bg-orange-500 dark:bg-orange-500 dark:hover:bg-orange-600">
+                <UIcon dynamic :name="!addedToCart ? 'i-material-symbols:add-shopping-cart-outline-rounded' : 'i-material-symbols:shopping-cart-off-outline'" class="text-lg font-semibold"/>
                 <span>{{ cartActionButton() }}</span>
             </button>
         </div>
@@ -92,5 +95,7 @@ const addToCart = () => {
 </template>
 
 <style scoped>
-
+.meta-action {
+    @apply w-full py-2 px-3 text-center transition-all text-white rounded-lg shadow-lg z-10 flex gap-x-3 justify-center items-center active:scale-95;
+}
 </style>
