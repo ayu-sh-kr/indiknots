58e8e63ee3110654a610dbc97cae9a9e54954d1a
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-15 12:25:03 +0530

    Update CartPreview.vue
    added additional data in cart preview

--- components/shop/cart/header/CartPreview.vue
+++ components/shop/cart/header/CartPreview.vue
@@ -20,15 +20,19 @@ const deleteFromCart = () => {
 </script>
 
 <template>
-<div class="py-1 px-3 transition-all hover:bg-gray-100 dark:hover:bg-gray-800
+<div class="py-1.5 px-3 transition-all hover:bg-gray-100 dark:hover:bg-gray-800
             cursor-pointer text-sm text-gray-800 dark:text-gray-200
             grid grid-cols-5 space-x-2 w-52"
 >
     <div class="w-6 h-8 col-span-1 overflow-hidden">
         <img :src="product.img" :alt="product.name" class="w-full h-full">
     </div>
-    <div class="col-span-3 flex flex-col justify-start items-start w-full">
-        <p class="text-xs line-clamp-1">{{product.name}}</p>
+    <div class="col-span-3 flex flex-col items-start gap-y-1 w-full">
+        <p class="text-xs font-semibold line-clamp-1">{{product.name}}</p>
+        <div class="text-xs flex justify-start items-center gap-x-2">
+            <span>{{product.getSizeText(product.size[0])}}</span>
+            <span>{{product.getMaterialText()}}</span>
+        </div>
     </div>
     <div class="col-span-1 flex justify-center items-center w-full">
         <button class="active:scale-95 transition-all" @click="deleteFromCart">
