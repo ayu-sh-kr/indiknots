e422a34ebfcb9bdb1ce57275c05d28ea5a5d6409
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-16 17:22:31 +0530

    Create pagination.store.ts
    store to manage page data for all the pagination context,
    added productPage ref to manage the page context for PageSection

---
+++ stores/pagination.store.ts
@@ -0,0 +1,12 @@
+import {defineStore} from "pinia";
+import {ref} from "vue";
+
+export const usePaginationStore = defineStore('pagination', () => {
+    const productPage = ref<number>(1);
+
+    const updateProductPage = (page: number) => {
+        productPage.value = page;
+    }
+
+    return {productPage, updateProductPage}
+})
