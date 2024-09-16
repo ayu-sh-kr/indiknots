fd6bed7cef404176cbe2beb54a607d505f77b95d
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-16 23:10:41 +0530

    Create ProductImagePreview.vue
    image preview in the carousel for image show case -> slide to given image by click

---
+++ components/shop/details/ProductImagePreview.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+    const props = defineProps({
+        image: {
+            type: String,
+            required: true
+        },
+
+        index: {
+            type: Number,
+            required: true
+        },
+
+        current: {
+            type: Number,
+            required: true
+        }
+    });
+
+    const emit = defineEmits(['image-selected']);
+
+    const updatedSelected = () => {
+        emit("image-selected", props.index);
+    }
+
+</script>
+
+<template>
+<div @click="updatedSelected" class="w-full h-36 overflow-hidden cursor-pointer" :class="`${current === index ? 'ring-2 ring-orange-400 dark:ring-orange-500' : ''}`">
+    <img :src="image" alt="product-image" class="w-full h-full">
+</div>
+</template>
+
+<style scoped>
+
+</style>
