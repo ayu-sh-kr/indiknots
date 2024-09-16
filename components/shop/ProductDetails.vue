b7efedcd506fe177bba51afa2bfc49bdd6114b26
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-16 23:09:43 +0530

    Update ProductDetails.vue
    update product page UI, change the image showcase for product

--- components/shop/ProductDetails.vue
+++ components/shop/ProductDetails.vue
@@ -2,6 +2,7 @@
 import NoContent from "~/components/utils/NoContent.vue";
 import ProductMeta from "~/components/shop/details/ProductMeta.vue";
 import type {ProductModal} from "~/modals/product.modal";
+import ProductImage from "~/components/shop/details/ProductImage.vue";
 
 const props = defineProps({
     product: {
@@ -14,9 +15,7 @@ const props = defineProps({
 
 <template>
 <div v-if="product" class="relative grid grid-cols-1 md:grid-cols-2 mt-10 tracking-wider place-items-start gap-y-10 px-5">
-    <div class="flex flex-col items-center gap-y-3 w-full overflow-scroll">
-        <img :src="product.img" :alt="product.name" class="w-[460px]">
-    </div>
+    <ProductImage/>
     <ProductMeta :product="product" />
 </div>
 <NoContent v-else />
