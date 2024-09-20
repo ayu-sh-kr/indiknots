94aa16389fc78ff508e5949c394c82eb3896572e
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-20 18:03:53 +0530

    Update ProductImage.vue
    changes and configuration to adapt new image modal

--- components/shop/details/ProductImage.vue
+++ components/shop/details/ProductImage.vue
@@ -2,12 +2,12 @@
 
 import ProductImagePreview from "~/components/shop/details/ProductImagePreview.vue";
 
-const images: string[] =  [
-    'https://www.rugberry.in/cdn/shop/files/DSC07485Large.jpg?v=1711915197&width=1000',
-    'https://www.rugberry.in/cdn/shop/products/4437702e93af682a2668a16f6b5e89b7.png?v=1711946936&width=500',
-    'https://www.rugberry.in/cdn/shop/products/97989055_3101840199840161_1935639458481176576_n.jpg?v=1718532281&width=1000',
-    'https://www.rugberry.in/cdn/shop/products/23.jpg?v=1711772575&width=1000'
-];
+const props = defineProps({
+    images: {
+        type: Object as PropType<ProductImage[]>,
+        required: true
+    }
+})
 
 const carouselRef = ref();
 const selected = ref<number>(0);
@@ -16,17 +16,19 @@ const onImageSelection = (data: number) => {
     selected.value = data;
     carouselRef.value.select(selected.value + 1);
 }
+
+console.log(props.images)
 </script>
 
 <template>
-    <div class="grid grid-cols-5 md:grid-cols-6 w-full gap-x-2 overflow-hidden">
+    <div class="grid grid-cols-5 md:grid-cols-6 w-full h-5/6 gap-x-2 overflow-hidden">
         <div class="hidden md:flex col-span-1 flex-col gap-y-10 p-2">
-            <ProductImagePreview v-for="(image, index) in images" :image="image" :index="index" :current="selected" @image-selected="onImageSelection"/>
+            <ProductImagePreview v-for="(image, index) in images" :image="image.url" :index="index" :current="selected" @image-selected="onImageSelection"/>
         </div>
 
         <div class="col-span-5 p-2 h-full">
-            <UCarousel ref="carouselRef" v-slot="{ item }" :items="images" :ui="{ item: 'basis-full' }" class="rounded-lg overflow-hidden">
-                <img :src="item" alt="product-image" class="w-full h-full" draggable="false">
+            <UCarousel ref="carouselRef" v-slot="{ item }" :items="images" :ui="{ item: 'basis-full' }" class="rounded-lg h-5/6 overflow-hidden">
+                <img :src="item.url" :alt="item.text" class="w-full h-full" draggable="false">
             </UCarousel>
         </div>
 
