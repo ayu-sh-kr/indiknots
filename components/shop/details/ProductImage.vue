d6fa33fa7af13caafde00f25a45fe909ff06e6da
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-21 22:15:26 +0530

    Update ProductImage.vue
    size changes for image in product meta

--- components/shop/details/ProductImage.vue
+++ components/shop/details/ProductImage.vue
@@ -21,14 +21,14 @@ console.log(props.images)
 </script>
 
 <template>
-    <div class="grid grid-cols-5 md:grid-cols-6 w-full h-5/6 gap-x-2 overflow-hidden">
+    <div class="grid grid-cols-5 md:grid-cols-6 place-content-start w-full h-5/6 gap-x-2 overflow-hidden">
         <div class="hidden md:flex col-span-1 flex-col gap-y-10 p-2">
             <ProductImagePreview v-for="(image, index) in images" :image="image.url" :index="index" :current="selected" @image-selected="onImageSelection"/>
         </div>
 
         <div class="col-span-5 p-2 h-full">
-            <UCarousel ref="carouselRef" v-slot="{ item }" :items="images" :ui="{ item: 'basis-full' }" class="rounded-lg h-5/6 overflow-hidden">
-                <img :src="item.url" :alt="item.text" class="w-full h-full" draggable="false">
+            <UCarousel ref="carouselRef" v-slot="{ item }" :items="images" :ui="{ item: 'basis-full' }" class="flex justify-center items-center overflow-hidden">
+                <img :src="item.url" :alt="item.text" class="w-5/6 h-5/6" draggable="false">
             </UCarousel>
         </div>
 
