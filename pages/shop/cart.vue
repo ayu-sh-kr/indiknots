b780314271ba2eb6b3f9c3e216d17ec5b12004b8
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-27 23:22:03 +0530

    Update cart.vue
    added cart body to cart page

--- pages/shop/cart.vue
+++ pages/shop/cart.vue
@@ -2,13 +2,14 @@
 
 import Scaffold from "~/components/utils/Scaffold.vue";
 import SectionHeader from "~/components/utils/SectionHeader.vue";
+import CartBody from "~/components/shop/cart/body/CartBody.vue";
 </script>
 
 <template>
     <Scaffold class="min-h-screen">
         <SectionHeader text2="Cart" text1=""/>
 
-
+        <CartBody/>
 
     </Scaffold>
 </template>
