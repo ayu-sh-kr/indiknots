b44ad5d5532bd46cbcb07fe424fcb65f20e9aff4
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-29 02:51:48 +0530

    Create InfoText.vue
    component for labeled text

---
+++ components/shop/cart/body/body/InfoText.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const props = defineProps({
+    heading: {
+        type: String,
+        required: true
+    },
+
+    details: {
+        type: String,
+        required: true
+    },
+
+    size: {
+        type: String
+    },
+});
+
+
+const Style = {
+    size: {
+        xs: 'text-xs',
+        sm: 'text-sm',
+        md: 'text-base',
+        lg: 'text-lg',
+        xl: 'text-xl'
+    }
+}
+
+type size = keyof typeof Style.size
+
+</script>
+
+<template>
+    <div class="flex gap-x-1 items-center text-nowrap dark:text-gray-300 text-gray-400" :class="`${props.size ? Style.size[props.size as size] : Style.size.xs}`">
+        <span class="font-semibold">{{heading}} - </span>
+        <span>{{details}}</span>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
