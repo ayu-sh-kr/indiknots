52638b4c1bb3d02fdaad4f57359c71f1bea67a1a
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-28 23:58:40 +0530

    Update cart.vue
    change background
    code refactoring

--- pages/shop/cart.vue
+++ pages/shop/cart.vue
@@ -6,11 +6,9 @@ import CartBody from "~/components/shop/cart/body/CartBody.vue";
 </script>
 
 <template>
-    <Scaffold class="min-h-screen">
+    <Scaffold class="min-h-screen dark:bg-gray-800 bg-gray-200">
         <SectionHeader text2="Cart" text1=""/>
-
         <CartBody/>
-
     </Scaffold>
 </template>
 
