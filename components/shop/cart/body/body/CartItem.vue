7b41274cd06d970e44a7e286a44e9854915b85ea
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-29 22:18:27 +0530

    Update CartItem.vue
    create new event to notify cart body for the changes in the item quantity

--- components/shop/cart/body/body/CartItem.vue
+++ components/shop/cart/body/body/CartItem.vue
@@ -3,6 +3,7 @@
 import {cartAction2Handler, type CartModal} from "~/modals/cart.modal";
 import ItemCount from "~/components/shop/cart/body/body/ItemCount.vue";
 import InfoText from "~/components/shop/cart/body/body/InfoText.vue";
+import {roundedTo2} from "../../../../../utils/GeneralUtils";
 
 const props = defineProps({
     item: {
@@ -13,6 +14,8 @@ const props = defineProps({
 
 const selectedSize = ref();
 const quantity = ref(1)
+
+const emit = defineEmits(['update-total']);
 
 onMounted(() => {
     selectedSize.value = props.item.selectedSize();
@@ -25,6 +28,7 @@ watch((selectedSize), (value: ProductSizeOption) => {
 
 const updatedQuantity = (value: number) => {
     quantity.value = value
+    emit("update-total")
 }
 
 const removeFromCart = () => {
@@ -51,8 +55,8 @@ const removeFromCart = () => {
             </div>
             <InfoText details="Indiknots" heading="Seller" size="sm"/>
             <div class="flex items-baseline gap-x-3 flex-wrap">
-                <span class="text-gray-600 dark:text-gray-300 line-through">${{Math.round(item.price.price * quantity * 100) / 100}}</span>
-                <span class="text-gray-700 dark:text-gray-200 text-xl font-semibold">${{item.product.getDiscountedPrice(item.price) * quantity}}</span>
+                <span class="text-gray-600 dark:text-gray-300 line-through">${{roundedTo2(item.price.price * quantity)}}</span>
+                <span class="text-gray-700 dark:text-gray-200 text-xl font-semibold">${{roundedTo2(item.product.getDiscountedPrice(item.price) * quantity)}}</span>
                 <span class="text-orange-400 dark:text-orange-500 text-sm">{{item.price.sale_percentage}}% OFF</span>
                 <span class="text-xs text-orange-400 dark:text-orange-500">DISCOUNT APPLIED</span>
             </div>
@@ -62,7 +66,7 @@ const removeFromCart = () => {
                 </div>
             </div>
             <div class="flex justify-end items-center gap-x-5 w-full">
-                <button @click="removeFromCart()" class="bg-beige-400 dark:bg-beige-500 cart-action-button">
+                <button @click="removeFromCart()" class="bg-rose-400 dark:bg-rose-500 cart-action-button">
                     <span>Remove From Cart</span>
                 </button>
                 <button class="bg-orange-400 dark:bg-orange-500 cart-action-button">
