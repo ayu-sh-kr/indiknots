b81f916f2e4ca288c254aea2918d1210b2790c56
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-29 22:17:55 +0530

    Update CartBody.vue
    added ui for the price information -> total price, total items, discount, delivery charges and platform fee,
    added method to update UI for changes in the quantity of the method

--- components/shop/cart/body/CartBody.vue
+++ components/shop/cart/body/CartBody.vue
@@ -1,30 +1,66 @@
 <script setup lang="ts">
 
-import type {CartModal} from "~/modals/cart.modal";
+import {type CartModal, getDiscountedPrice, getTotalPrice} from "~/modals/cart.modal";
 import {useCartStore} from "~/stores/cart.store";
 import CartItem from "~/components/shop/cart/body/body/CartItem.vue";
 
 
 const cart = ref<CartModal[]>([])
 const cartStore = useCartStore()
+const totalPrice = ref()
+const discounted = ref()
 
 onMounted(() => {
     cart.value = cartStore.cart
+    totalPrice.value = getTotalPrice(cart.value)
+    discounted.value = getDiscountedPrice(cart.value)
 });
 
 watch(() => cartStore.cart, (newProducts) => {
     cart.value = newProducts
+    updateTotal();
+    updateDiscounted();
 });
+
+const updateTotal = () => {
+  totalPrice.value = roundedTo2(getTotalPrice(cart.value));
+}
+
+const updateDiscounted = () => {
+  discounted.value = roundedTo2(getDiscountedPrice(cart.value))
+}
 </script>
 
 <template>
 
-    <section class="grid md:grid-cols-8 max-w-7xl mx-auto place-items-center">
+    <section class="grid md:grid-cols-8 max-w-7xl mx-auto font-tahoma">
         <div class="col-span-6 space-y-5 p-2">
-            <CartItem v-for="(item, index) in cart" :key="index" :item="item"/>
+            <CartItem v-for="(item, index) in cart" :key="index" :item="item" @update-total="updateTotal(); updateDiscounted()"/>
         </div>
-        <div class="col-span-2">
-
+        <div class="col-span-2 flex justify-center p-2">
+            <div class="w-full bg-white dark:bg-slate-800 text-gray-700 dark:text-gray-300 rounded-xl space-y-2 tracking-wide p-2">
+                <h4 class="text-xl border-b py-2 px-3">Price Details</h4>
+                <div class="flex justify-between items-center w-full py-1.5 px-3">
+                    <h4>Price ( {{cart.length}} Items)</h4>
+                    <h4>${{totalPrice}}</h4>
+                </div>
+                <div class="flex justify-between items-center w-full py-1.5 px-3">
+                    <h4>Discount</h4>
+                    <h4 class="text-orange-400 dark:text-orange-500">-${{roundedTo2(totalPrice - discounted)}}</h4>
+                </div>
+                <div class="flex justify-between items-center w-full py-1.5 px-3">
+                    <h4>Platform Fee</h4>
+                    <h4 class="text-orange-400 dark:text-orange-500">0</h4>
+                </div>
+                <div class="flex justify-between items-center w-full py-1.5 px-3">
+                    <h4>Delivery Charges</h4>
+                    <h4 class="text-orange-400 dark:text-orange-500">Free</h4>
+                </div>
+                <div class="flex justify-between items-center w-full py-2 px-3 text-xl font-semibold border-y-2 border-dashed">
+                    <h4>Total Amount</h4>
+                    <h4>${{discounted}}</h4>
+                </div>
+            </div>
         </div>
     </section>
 
