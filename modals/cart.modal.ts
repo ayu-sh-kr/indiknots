b9b17d9fa937ec6813e59e33ea1692f514c98f5d
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-30 20:18:48 +0530

    Update cart.modal.ts
    added ts docs for maintainability

--- modals/cart.modal.ts
+++ modals/cart.modal.ts
@@ -1,5 +1,9 @@
 import type {ProductModal} from "~/modals/product.modal";
 
+/**
+ * Represents an item in the shopping cart.
+ * @implements CartItem - Interface for the Cart Data
+ */
 class CartModal implements CartItem {
     color!: string;
     quantity!: number;
@@ -8,16 +12,27 @@ class CartModal implements CartItem {
     size!: ProductSize;
     product!: ProductModal
 
+    /**
+     * Increments the quantity of the product in the cart by 1.
+     */
     incrementCount = () => {
         this.quantity += 1;
         console.log(this.quantity)
     }
 
+    /**
+     * Decrements the quantity of the product in the cart by 1.
+     * Ensures that the quantity does not go below 1.
+     */
     decrementCount = () => {
         if (this.quantity === 1) return;
         this.quantity -= 1;
     }
 
+    /**
+     * Gets the selected size of the product as a `ProductSizeOption`.
+     * @returns The selected size option.
+     */
     selectedSize = () => {
         const sizeOption: ProductSizeOption = {
             label: this.product.getSizeText(this.size),
@@ -26,12 +41,20 @@ class CartModal implements CartItem {
         return sizeOption
     }
 
+    /**
+     * Updates the selected size based on the new size selected.
+     * @param option - The new size of the product in the form of `ProductSizeOption`.
+     */
     updateSelectedSize = (option: ProductSizeOption) => {
         this.size = option.value;
         this.updatePrice(this.size)
     }
 
-    updatePrice = (size: ProductSize) => {
+    /**
+     * Update the price of the product based on the selected size.
+     * @param size - The new size of the product
+     */
+    private updatePrice = (size: ProductSize) => {
         const price = this.product.getPrizeBySize(size)
 
         if(price) {
@@ -93,6 +116,15 @@ class CartModalBuilder {
 }
 
 
+/**
+ * Handles the action of adding or removing a product from the cart.
+ * If the product does not exist in the cart and is available in stock, it adds the product to the cart.
+ * If the product already exists in the cart, it removes the product from the cart.
+ *
+ * @param cartModal - The `CartModal` instance representing the product to be added or removed.
+ * @param cartStore - The `CartStore` instance managing the cart state.
+ * @returns `true` if the product was added to the cart, `false` if the product was removed from the cart.
+ */
 const cartAction2Handler = (cartModal: CartModal, cartStore: CartStore) => {
     if(!cartStore().isProductExist(cartModal.productId) && cartModal.product.stock === "AVAILABLE") {
         cartStore().addToCart(cartModal)
@@ -103,6 +135,17 @@ const cartAction2Handler = (cartModal: CartModal, cartStore: CartStore) => {
     }
 }
 
+
+/**
+ * Calculates the total price of all items in the cart.
+ *
+ * This function iterates over each `CartModal` item in the provided array,
+ * multiplies the price of each item by its quantity, and sums up these values
+ * to get the total price.
+ *
+ * @param cartModals - An array of `CartModal` instances representing the items in the cart.
+ * @returns The total price of all items in the cart.
+ */
 const getTotalPrice = (cartModals: CartModal[]) => {
     let price = 0;
     cartModals.forEach(modal => {
@@ -112,6 +155,17 @@ const getTotalPrice = (cartModals: CartModal[]) => {
     return price;
 }
 
+
+/**
+ * Calculates the total discounted price of all items in the cart.
+ *
+ * This function iterates over each `CartModal` item in the provided array,
+ * multiplies the discounted price of each item by its quantity, and sums up these values
+ * to get the total discounted price.
+ *
+ * @param cartModals - An array of `CartModal` instances representing the items in the cart.
+ * @returns The total discounted price of all items in the cart.
+ */
 const getDiscountedPrice = (cartModals: CartModal[]) => {
     let discounted = 0;
     cartModals.forEach(modal => {
