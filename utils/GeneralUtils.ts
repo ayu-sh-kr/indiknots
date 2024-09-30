e4e7e594de35d1700f0e5bf28b9f4c1983242fd3
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-30 23:57:19 +0530

    Update GeneralUtils.ts
    added ts docs

--- utils/GeneralUtils.ts
+++ utils/GeneralUtils.ts
@@ -1,4 +1,9 @@
 
+/**
+ * `Function` to round off a decimal number to 2 digit.
+ * @param value - `number` to round off to 2-digit decimal value.
+ * @returns number - value rounded off to 2 decimal.
+ */
 const roundedTo2 = (value: number) => {
     return Math.round(value * 100) / 100;
 }
