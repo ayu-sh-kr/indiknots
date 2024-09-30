4450a6b90a077cd78433eef635115e8fc82c599c
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-01 01:16:55 +0530

    Update pagination.store.ts
    added product sort to the pagination store to remember it even if the configuration or route changes

--- stores/pagination.store.ts
+++ stores/pagination.store.ts
@@ -3,10 +3,15 @@ import {ref} from "vue";
 
 export const usePaginationStore = defineStore('pagination', () => {
     const productPage = ref<number>(1);
+    const productSort = ref<ProductSort>("None");
 
     const updateProductPage = (page: number) => {
         productPage.value = page;
     }
 
-    return {productPage, updateProductPage}
+    const updateProductSort = (sort: ProductSort) => {
+        productSort.value = sort;
+    }
+
+    return {productPage, productSort, updateProductPage, updateProductSort}
 });
