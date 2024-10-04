b1c182ca4cb0ed29a5649a60f059484cbd8aba19
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-04 23:46:51 +0530

    Update FilterAction.vue
    completed the FilterAction UI,
    next task is to implement the logic

--- components/shop/product/FilterAction.vue
+++ components/shop/product/FilterAction.vue
@@ -8,16 +8,35 @@ const props = defineProps({
         required: true,
         default: false
     }
-})
+});
 
 </script>
 
 <template>
-<div class="overflow-hidden p-2 grid grid-cols-6 gap-x-5 gap-y-3 text-gray-800 dark:text-gray-200">
+<div class="overflow-hidden p-2 grid grid-cols-6 gap-x-5 gap-y-3 text-gray-800 dark:text-gray-200 bg-slate-100 dark:bg-slate-900 py-4 rounded-xl">
     <USelectMenu placeholder="Choose Category"/>
     <USelectMenu placeholder="Choose Technique" />
     <USelectMenu placeholder="Choose Material" />
-    <RangeUI :max="10000" :min="0" />
+    <UPopover>
+        <UButton color="gray" class="w-full flex items-center justify-center">Low to High (Price)</UButton>
+        <template #panel>
+            <div class="w-56 p-2">
+                <RangeUI :max="10000" :min="0" />
+            </div>
+        </template>
+    </UPopover>
+    <UPopover>
+        <UButton color="gray" class="w-full flex items-center justify-center">High to Low (Price)</UButton>
+        <template #panel>
+            <div class="w-56 p-2">
+                <RangeUI :max="10000" :min="0" />
+            </div>
+        </template>
+    </UPopover>
+
+    <UButton class="flex justify-center active:scale-95 transition-all">
+        Apply
+    </UButton>
 </div>
 </template>
 
