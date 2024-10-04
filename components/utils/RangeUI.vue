c8a4fc0fb665f5200b5d52ff16ad773f35b0fc98
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-04 23:22:14 +0530

    Create RangeUI.vue
    apply range filter

---
+++ components/utils/RangeUI.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    min: {
+        type: Number,
+        required: true
+    },
+    max: {
+        type: Number,
+        required: true
+    }
+});
+
+const current = ref<number>(0);
+
+const emit = defineEmits(['range-change'])
+
+watch((current), (value) => {
+    emit("range-change", value);
+})
+
+</script>
+
+<template>
+<div class="flex flex-col items-center text-sm">
+    <div class="flex justify-between items-center w-full">
+        <p>${{min}}</p>
+        ${{current}}
+        <p>${{max}}</p>
+    </div>
+    <URange :step="50" :min="min" :max="max" v-model="current"/>
+</div>
+</template>
+
+<style scoped>
+
+</style>
