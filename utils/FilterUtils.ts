1a1f1039883324452dacc018adc9c900fe087178
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-05 01:27:59 +0530

    Create FilterUtils.ts
    implemented filter functionality in a separate file for better code quality

---
+++ utils/FilterUtils.ts
@@ -0,0 +1,66 @@
+import type {ProductModal} from "~/modals/product.modal";
+
+
+/**
+ * Filters an array of ProductModal instances based on the specified categories.
+ *
+ * @param products - The array of `ProductModal` instances to be filtered.
+ * @param categories - The array of `ProductCategories` to filter the products by.
+ * @returns The filtered array of ProductModal instances that belong to the specified categories.
+ */
+const filterByCategories = (products: ProductModal[], categories: ProductCategory[]): ProductModal[] => {
+    return products.filter(product => {
+        return categories.includes(product.category);
+    })
+}
+
+
+/**
+ * Filters an array of ProductModal instances based on the specified materials.
+ *
+ * @param  products - The array of `ProductModal` instances to be filtered.
+ * @param  materials - The array of `ProductMaterials` to filter the products by.
+ * @returns The filtered array of ProductModal instances that are made of the specified materials.
+ */
+const filterByMaterials = (products: ProductModal[], materials: ProductMaterial[]): ProductModal[] => {
+    return products.filter(product => {
+        return materials.includes(product.material)
+    })
+}
+
+
+/**
+ * Filters an array of ProductModal instances based on the specified techniques.
+ *
+ * @param products - The array of `ProductModal` instances to be filtered.
+ * @param techniques - The array of `ProductTechniques` to filter the products by.
+ * @returns The filtered array of ProductModal instances that use the specified techniques.
+ */
+const filterByTechniques = (products: ProductModal[], techniques: ProductTechnique[]) => {
+    return products.filter(product => {
+        return techniques.includes(product.technique)
+    });
+}
+
+
+
+/**
+ * Filters an array of ProductModal instances based on the specified filter criteria.
+ * The function applies category, material, and technique filters separately and then
+ * takes the intersection of the filtered results.
+ *
+ * @param filter - The filter criteria containing arrays of categories, materials, and techniques.
+ * @param products - The array of ProductModal instances to be filtered.
+ * @returns The filtered array of ProductModal instances that match all the specified filter criteria.
+ */
+const handleProductFiltering = (filter: ProductFilter, products: ProductModal[]) => {
+    const filteredByCategories = filterByCategories(products, filter.category);
+    const filteredByMaterials = filterByMaterials(products, filter.material);
+    const filteredByTechniques = filterByTechniques(products, filter.technique);
+
+    return filteredByCategories.filter(product => {
+        return filteredByMaterials.includes(product) && filteredByTechniques.includes(product);
+    });
+}
+
+export {handleProductFiltering}
