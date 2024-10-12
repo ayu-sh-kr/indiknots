d335d737317d41685088678d1e60b11751980e65
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-12 16:04:33 +0530

    Create security.vue
    created new layout for login, register and other pages where main header is not required

---
+++ layouts/security.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import SecurityHeader from "~/components/utils/application/SecurityHeader.vue";
+</script>
+
+<template>
+    <SecurityHeader/>
+    <main class="font-dm antialiased">
+        <slot/>
+    </main>
+</template>
+
+<style scoped>
+
+</style>
