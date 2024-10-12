1b51cfceddbec16889d1822d28f610dbcccfacde
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-12 16:03:19 +0530

    Create SecurityHeader.vue
    create another header for pages such as login, register and more

---
+++ components/utils/application/SecurityHeader.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import AppLogo from "~/components/utils/application/AppLogo.vue";
+import AppDarkMode from "~/components/utils/application/AppDarkMode.vue";
+</script>
+
+<template>
+    <div class="fixed top-0 left-0 z-50 w-full flex justify-between bg-white dark:bg-black shadow-xl items-center py-3 px-4">
+        <AppLogo/>
+        <AppDarkMode/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
