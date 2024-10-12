a0032c8592ed3636e0c73d558c01392cd55bea21
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-12 16:03:00 +0530

    Create AppLogo.vue
    separated the logo from header for reusability

---
+++ components/utils/application/AppLogo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <NuxtLink :to="'/'">
+        <img :src="'/images/logo/logo-no-background.png'" alt="Indiknots Logo" class="h-20 w-20">
+    </NuxtLink>
+</template>
+
+<style scoped>
+
+</style>
