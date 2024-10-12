15f2e5309983005fe66b19d85d481a800ac6346d
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-12 16:02:46 +0530

    Create AppDarkMode.vue
    button to trigger dark and light mode with dynamic icon

---
+++ components/utils/application/AppDarkMode.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import {useDark} from "@vueuse/core";
+import {useToggle} from "@vueuse/shared";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
+
+<template>
+    <UButton dynamic
+             :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+             :color="isDark ? 'gray' : 'orange'"
+             variant="ghost"
+             @click="toggleDark()"
+    />
+</template>
+
+<style scoped>
+
+</style>
