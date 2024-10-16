b1ba78dfee67d77f28d6951e9b82a6dccccddb68
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-17 00:10:00 +0530

    Create PaymentPageRefer.vue
    reference links to payment related pages

---
+++ components/account/buttons/PaymentPageRefer.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="border-t border-adaptive">
+        <div class="flex justify-between items-center py-2 px-3 gap-x-5">
+            <UIcon name="i-fluent:payment-24-filled" class="sidebar-icon" color="orange" variant="soft"></UIcon>
+            <h4 class="flex-1 sidebar-text text-left">Payment Settings</h4>
+        </div>
+        <div>
+            <div class="sidebar-content sidebar-hover">
+                <p>Add Coupons</p>
+            </div>
+            <div class="sidebar-content sidebar-hover">
+                <p>Manage Gifts</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
