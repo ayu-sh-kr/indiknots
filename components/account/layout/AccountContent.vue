613270d03b9c51a15308e27bfaf3c9807aee1144
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-17 01:25:11 +0530

    Create AccountContent.vue
    wrapper for content on account layout

---
+++ components/account/layout/AccountContent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="bg-white dark:bg-gray-800 rounded-lg shadow-xl border-adaptive px-6 py-2 space-y-10 w-full">
+        <slot/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
