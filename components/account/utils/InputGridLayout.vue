549b0ea8ec76f2ec757128a64fce38274abfa384
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-17 00:27:21 +0530

    Create InputGridLayout.vue
    component layout for input sections on short forms

---
+++ components/account/utils/InputGridLayout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-10 gap-y-5 h-fit">
+    <slot/>
+</div>
+</template>
+
+<style scoped>
+
+</style>
