9f4779261d97d1651f1491af676d272b0d6195aa
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-17 01:06:32 +0530

    Update AccountPageRefer.vue
    modify component to show active link,
    fix issue where whole component was link instead of buttons

--- components/account/buttons/AccountPageRefer.vue
+++ components/account/buttons/AccountPageRefer.vue
@@ -1,24 +1,44 @@
 <script setup lang="ts">
 
+import {getActivePageInfo} from "~/utils/GeneralUtils";
+
+const links: Link = [
+    {
+        label: 'Personal Information',
+        to: '/account/profile'
+    },
+
+    {
+        label: 'Manage Addresses',
+        to: '/account/manage-addresses'
+    }
+];
+
+interface Link {
+    label: string,
+    to: string
+}
+
+const route = useRoute();
+const activeHeader = computed(() => getActivePageInfo(route, links));
 </script>
 
 <template>
-    <NuxtLink :to="'/account/profile'">
-        <div class="border-t border-adaptive">
-            <div class="flex justify-between items-center py-2 px-3 gap-x-5">
-                <UIcon name="i-material-symbols:person-4-rounded" class="sidebar-icon" color="orange" variant="soft"></UIcon>
-                <h4 class="flex-1 sidebar-text text-left">Account Settings</h4>
-            </div>
-            <div>
-                <div class="sidebar-content sidebar-hover">
-                    <p>Personal Information</p>
-                </div>
-                <div class="sidebar-content sidebar-hover">
-                    <p>Manage Addresses</p>
-                </div>
-            </div>
+    <div class="border-t border-adaptive">
+        <div class="flex justify-between items-center py-2 px-3 gap-x-5">
+            <UIcon name="i-material-symbols:person-4-rounded" class="sidebar-icon" color="orange"
+                   variant="soft"
+            />
+            <h4 class="flex-1 sidebar-text text-left">Account Settings</h4>
         </div>
-    </NuxtLink>
+        <div>
+            <NuxtLink :to="link.to" v-for="link in links">
+                <div class="sidebar-content sidebar-hover" :class="{'text-orange-400 dark:text-orange-500' : activeHeader.label === link.label}">
+                    <p>{{ link.label }}</p>
+                </div>
+            </NuxtLink>
+        </div>
+    </div>
 </template>
 
 <style scoped>
