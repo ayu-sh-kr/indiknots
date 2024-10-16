809ea4f0016ebd349999bc4096c8592d65ebefcb
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-17 00:43:31 +0530

    Update AccountPageRefer.vue
    connected by link profile page to button AccountPageRefer.vue

--- components/account/buttons/AccountPageRefer.vue
+++ components/account/buttons/AccountPageRefer.vue
@@ -3,20 +3,22 @@
 </script>
 
 <template>
-    <div class="border-t border-adaptive">
-        <div class="flex justify-between items-center py-2 px-3 gap-x-5">
-            <UIcon name="i-material-symbols:person-4-rounded" class="sidebar-icon" color="orange" variant="soft"></UIcon>
-            <h4 class="flex-1 sidebar-text text-left">Account Settings</h4>
-        </div>
-        <div>
-            <div class="sidebar-content sidebar-hover">
-                <p>Personal Information</p>
+    <NuxtLink :to="'/account/profile'">
+        <div class="border-t border-adaptive">
+            <div class="flex justify-between items-center py-2 px-3 gap-x-5">
+                <UIcon name="i-material-symbols:person-4-rounded" class="sidebar-icon" color="orange" variant="soft"></UIcon>
+                <h4 class="flex-1 sidebar-text text-left">Account Settings</h4>
             </div>
-            <div class="sidebar-content sidebar-hover">
-                <p>Manage Addresses</p>
+            <div>
+                <div class="sidebar-content sidebar-hover">
+                    <p>Personal Information</p>
+                </div>
+                <div class="sidebar-content sidebar-hover">
+                    <p>Manage Addresses</p>
+                </div>
             </div>
         </div>
-    </div>
+    </NuxtLink>
 </template>
 
 <style scoped>
