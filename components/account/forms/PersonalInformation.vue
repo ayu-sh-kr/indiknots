7b6facaaf4f779d800d5d9a4165ceee9b4357328
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-17 00:27:46 +0530

    Update PersonalInformation.vue
    added FormLayout and InputGridLayout

--- components/account/forms/PersonalInformation.vue
+++ components/account/forms/PersonalInformation.vue
@@ -2,6 +2,8 @@
 
 import EnrichInput from "~/components/account/utils/EnrichInput.vue";
 import SaveButton from "~/components/account/utils/SaveButton.vue";
+import FormLayout from "~/components/account/utils/ShortFormLayout.vue";
+import InputGridLayout from "~/components/account/utils/InputGridLayout.vue";
 
 const disabled = ref(true);
 
@@ -29,17 +31,17 @@ const updateGender = (value: string) => {
 </script>
 
 <template>
-    <div class="flex flex-col gap-y-5 font-adaptive">
+    <FormLayout>
         <div class="flex justify-start items-center gap-x-10">
             <h4 class="text-lg">Personal <span class="text-orange-400 dark:text-orange-500">Information</span></h4>
             <UButton @click="disabled = !disabled" color="orange" variant="link">{{ disabled ? 'Edit' : 'Cancel' }}</UButton>
         </div>
-        <div class="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-x-10 gap-y-5 h-fit">
+        <InputGridLayout>
             <EnrichInput label="First Name" value="Ayush" :disabled="disabled"/>
             <EnrichInput label="Middle Name" value="Kumar" :disabled="disabled"/>
             <EnrichInput label="Last Name" value="Jaiswal" :disabled="disabled"/>
             <SaveButton :hidden="disabled"/>
-        </div>
+        </InputGridLayout>
         <div class="flex flex-col gap-y-3">
             <span class="text-sm">Your Gender</span>
             <div class="flex gap-x-5 items-center">
@@ -54,7 +56,7 @@ const updateGender = (value: string) => {
                 />
             </div>
         </div>
-    </div>
+    </FormLayout>
 </template>
 
 <style scoped>
