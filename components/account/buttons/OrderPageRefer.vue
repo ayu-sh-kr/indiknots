6dcf5b8341f39d2f843aa79ed4d7edc85775fead
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-17 00:09:47 +0530

    Create OrderPageRefer.vue
    reference link to order page

---
+++ components/account/buttons/OrderPageRefer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="flex justify-between items-center py-2 px-3 gap-x-5">
+        <UIcon name="i-material-symbols:orders-rounded" class="sidebar-icon" color="orange" variant="soft"></UIcon>
+        <h4 class="flex-1 sidebar-text text-left">My Orders</h4>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
