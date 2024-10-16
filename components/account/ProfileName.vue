53077b22c0e504863db0f457e61261d2b28167df
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-17 00:11:05 +0530

    Create ProfileName.vue
    header to represent active account name in account page

---
+++ components/account/ProfileName.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="bg-white dark:bg-gray-800 rounded-lg py-2 px-3 flex justify-between gap-x-2 font-adaptive border-adaptive shadow-lg">
+        <UAvatar alt="Ayush Kumar" color="orange" variant="soft" size="lg"/>
+        <div class="flex-1">
+            <span class="text-xs text-orange-400 dark:text-orange-500">Hello,</span>
+            <h4 class="font-semibold">Ayush Kumar Jaiswal</h4>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
