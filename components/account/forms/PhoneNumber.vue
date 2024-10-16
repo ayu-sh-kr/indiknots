b1fe56af2eb626d367b7b9196a4f26c3fa412d4b
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-17 00:27:40 +0530

    Update PhoneNumber.vue
    added FormLayout and InputGridLayout

--- components/account/forms/PhoneNumber.vue
+++ components/account/forms/PhoneNumber.vue
@@ -2,21 +2,23 @@
 
 import EnrichInput from "~/components/account/utils/EnrichInput.vue";
 import SaveButton from "~/components/account/utils/SaveButton.vue";
+import FormLayout from "~/components/account/utils/ShortFormLayout.vue";
+import InputGridLayout from "~/components/account/utils/InputGridLayout.vue";
 
 const disabled = ref(true);
 </script>
 
 <template>
-    <div class="flex flex-col gap-y-5 font-adaptive">
+    <FormLayout>
         <div class="flex justify-start items-center gap-x-10">
             <h4 class="text-lg">Phone <span class="text-orange-400 dark:text-orange-500">Number</span></h4>
             <UButton @click="disabled = !disabled" color="orange" variant="link">{{ disabled ? 'Edit' : 'Cancel' }}</UButton>
         </div>
-        <div class="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-x-10">
+        <InputGridLayout>
             <EnrichInput label="Phone (add country code)" :disabled="disabled" value="+918931007054" />
             <SaveButton :hidden="disabled"/>
-        </div>
-    </div>
+        </InputGridLayout>
+    </FormLayout>
 </template>
 
 <style scoped>
