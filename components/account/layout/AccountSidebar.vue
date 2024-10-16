8a355675edf5023135b527d4a3ec2990483877a7
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-10-17 00:43:59 +0530

    Create AccountSidebar.vue
    for UI enhancement and more control move side bar of account page to separate component

---
+++ components/account/layout/AccountSidebar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import OrderPageRefer from "~/components/account/buttons/OrderPageRefer.vue";
+import AccountPageRefer from "~/components/account/buttons/AccountPageRefer.vue";
+import PaymentPageRefer from "~/components/account/buttons/PaymentPageRefer.vue";
+import ProfileName from "~/components/account/ProfileName.vue";
+</script>
+
+<template>
+    <ProfileName/>
+
+    <div class="mt-10 bg-white dark:bg-gray-800 rounded-lg shadow-lg font-adaptive border-adaptive overflow-hidden">
+        <OrderPageRefer/>
+        <AccountPageRefer/>
+        <PaymentPageRefer/>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
