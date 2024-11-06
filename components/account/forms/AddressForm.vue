0763200fb3fc307bd6e7ebf25a314381bc7e3b8c
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-06 11:46:01 +0530

    component to create an address using the form

---
+++ components/account/forms/AddressForm.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+
+import ShortFormLayout from "~/components/account/layout/ShortFormLayout.vue";
+import InputGridLayout from "~/components/account/utils/InputGridLayout.vue";
+import EnrichInput from "~/components/account/utils/EnrichInput.vue";
+
+const formOpen = ref(false)
+</script>
+
+<template>
+    <div v-if="!formOpen"
+         @click="formOpen = !formOpen"
+        class="border-adaptive py-2 px-3 text-sm font-medium text-orange-400 dark:text-orange-500 flex justify-start items-center gap-x-5 cursor-pointer">
+        <Icon name="i-heroicons:plus" class="text-xl"/>
+        <h4>ADD A NEW ADDRESS</h4>
+    </div>
+
+    <div v-else class="border-adaptive bg-orange-100/20 dark:bg-orange-900/10 px-3 py-2">
+        <h4 class="text-sm text-orange-400 dark:text-orange-500">ADD A NEW ADDRESS</h4>
+
+        <ShortFormLayout class="mt-5">
+            <InputGridLayout>
+                <EnrichInput label="Name" :disabled="false" value="" @value-change=""/>
+                <EnrichInput label="10-digit mobile number" :disabled="false" value="" @value-change=""/>
+            </InputGridLayout>
+
+            <InputGridLayout>
+                <EnrichInput label="Pincode" :disabled="false" value="" @value-change=""/>
+                <EnrichInput label="Locality" :disabled="false" value="" @value-change=""/>
+            </InputGridLayout>
+
+            <InputGridLayout>
+                <EnrichInput class="lg:col-span-4" label="Address (Area and Street)" :disabled="false" value="" @value-change=""/>
+            </InputGridLayout>
+
+            <InputGridLayout>
+                <EnrichInput label="City" :disabled="false" value="" @value-change=""/>
+                <EnrichInput label="State" :disabled="false" value="" @value-change=""/>
+            </InputGridLayout>
+
+            <InputGridLayout>
+                <EnrichInput label="Landmark (Optional)" :disabled="false" value="" @value-change=""/>
+                <EnrichInput label="Alternate Phone (Optional)" :disabled="false" value="" @value-change=""/>
+            </InputGridLayout>
+
+            <InputGridLayout>
+                <button class="px-3 py-2 w-full bg-orange-400 dark:bg-orange-500 text-white font-medium">Save</button>
+                <button @click="formOpen = !formOpen" class="px-3 py-2 w-full font-adaptive bg-slate-200 dark:bg-slate-700">Close</button>
+            </InputGridLayout>
+        </ShortFormLayout>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
