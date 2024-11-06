681c56b29c67c23f250a7576f18216c7e4d49425
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-06 11:49:18 +0530

    fix layout for Enriched Input component

--- components/account/utils/InputGridLayout.vue
+++ components/account/utils/InputGridLayout.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-<div class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-10 gap-y-5 h-fit">
+<div class="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-10 gap-y-5 h-fit">
     <slot/>
 </div>
 </template>
