63da8cee25f93b38226f9982c910dbfdf271a066
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-06 11:47:32 +0530

    Component to create label for adding to the form on personal information page

---
+++ components/account/utils/AccountLabel.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps({
+    primary: String,
+    secondary: String
+})
+</script>
+
+<template>
+    <h4 class="text-lg">{{ primary }} <span class="text-orange-400 dark:text-orange-500">{{ secondary }}</span></h4>
+</template>
