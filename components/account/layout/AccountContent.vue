525590dcf69ad8deebdf94784f3745923a414f92
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-06 11:48:30 +0530

    added adaptive font style

--- components/account/layout/AccountContent.vue
+++ components/account/layout/AccountContent.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div class="bg-white dark:bg-gray-800 rounded-lg shadow-xl border-adaptive px-6 py-2 space-y-10 w-full">
+    <div class="bg-white dark:bg-gray-800 rounded-lg shadow-xl border-adaptive font-adaptive px-6 py-2 space-y-10 w-full">
         <slot/>
     </div>
 </template>
