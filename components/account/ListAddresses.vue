151d83babc7e5c9462bddfa1f3556034e9034b1f
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-08 02:49:41 +0530

    Added popover and buttons to edit or delete address

--- components/account/ListAddresses.vue
+++ components/account/ListAddresses.vue
@@ -21,7 +21,19 @@ watch((accountStore.addresses), (newValue) => {
 <template>
     <div>
         <div v-for="(address, index) in addresses" :key="index" class="border-adaptive px-3 py-2 space-y-3 text-sm">
-            <UBadge :label="address.addressType" color="gray" variant="soft"/>
+            <div class="flex justify-between items-center">
+                <UBadge :label="address.addressType" color="gray" variant="soft"/>
+                <UPopover>
+                    <UButton icon="i-mdi:dots-vertical" color="gray" variant="ghost"/>
+
+                    <template #panel>
+                        <div class="flex flex-col w-24">
+                            <UButton label="Edit" color="gray" variant="ghost" icon="i-material-symbols:edit" />
+                            <UButton label="Delete" color="gray" variant="ghost" icon="i-material-symbols:delete"/>
+                        </div>
+                    </template>
+                </UPopover>
+            </div>
 
             <div class="flex justify-start items-center gap-x-5 font-semibold">
                 <p>{{address.name}}</p>
