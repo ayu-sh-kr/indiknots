4dc93330d52e0837ce4fec16c26047a02e4fb340
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-08 21:14:52 +0530

    unit component to render address value and provide feature to edit and delete

---
+++ components/account/address/AddressView.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+
+import AddressForm from "~/components/account/forms/AddressForm.vue";
+import type {AddressModal} from "~/modals/address.modal";
+
+const props = defineProps({
+    address: {
+        type: Object as PropType<AddressModal>,
+        required: true
+    }
+});
+
+const formOpen = ref(false)
+
+const addressForm = reactive<Address>(
+    props.address
+)
+
+const close = () => {
+    formOpen.value = !formOpen.value;
+}
+
+const submit = () => {
+    close();
+}
+
+</script>
+
+<template>
+    <div v-if="!formOpen" class="border-adaptive px-3 py-2 space-y-3 text-sm">
+        <div class="flex justify-between items-center">
+            <UBadge :label="address.addressType" color="gray" variant="soft"/>
+            <UPopover>
+                <UButton icon="i-mdi:dots-vertical" color="gray" variant="ghost"/>
+
+                <template #panel>
+                    <div class="flex flex-col w-24">
+                        <UButton @click="formOpen = !formOpen" label="Edit" color="gray" variant="ghost" icon="i-material-symbols:edit" />
+                        <UButton label="Delete" color="gray" variant="ghost" icon="i-material-symbols:delete"/>
+                    </div>
+                </template>
+            </UPopover>
+        </div>
+
+        <div class="flex justify-start items-center gap-x-5 font-semibold">
+            <p>{{address.name}}</p>
+            <p>{{address.phone.number}}</p>
+        </div>
+
+        <div class="space-y-1">
+            <p>{{address.area}}</p>
+            <p>{{address.getAdministrativeAddress()}}</p>
+        </div>
+    </div>
+
+    <AddressForm v-else :submit="submit" :address="addressForm" @close-form="close"/>
+</template>
+
+<style scoped>
+
+</style>
