0ff06d1268b4a6539820c3b3472456f944e3c132
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-10 01:39:50 +0530

    added create password tab

--- pages/register.vue
+++ pages/register.vue
@@ -3,6 +3,7 @@
 import Scaffold from "~/components/utils/Scaffold.vue";
 import EmailRegisterTile from "~/components/register/tiles/EmailRegisterTile.vue";
 import OtpVerificationTile from "~/components/register/tiles/OtpVerificationTile.vue";
+import CreatePasswordTile from "~/components/register/tiles/CreatePasswordTile.vue";
 
 definePageMeta({
     layout: 'security'
@@ -17,12 +18,17 @@ const items = [
     {
         key: 'email',
         label: 'Email Registeration',
-        icon: 'i-heroicons-information-circle'
+        icon: 'i-material-symbols:mail-rounded'
     },
     {
         key: 'otp',
         label: 'OTP Verification',
-        icon: 'i-heroicons-arrow-down-tray'
+        icon: 'i-teenyicons:otp-solid',
+    },
+    {
+        key: 'password',
+        label: 'Create Password',
+        icon: 'i-material-symbols:lock',
     }
 ];
 
@@ -34,10 +40,14 @@ const tabIndex = ref(0);
 <template>
 
     <Scaffold class="flex items-center justify-center h-screen w-full font-adaptive">
-        <UTabs :items="items" v-model="tabIndex">
+        <UTabs :items="items" v-model="tabIndex" class="w-full md:w-2/3 lg:w-1/3">
+            <template #icon="{ item, selected }">
+                <UIcon :name="item.icon" class="w-4 h-4 flex-shrink-0 me-2" :class="[selected && 'text-primary-500 dark:text-primary-400']" />
+            </template>
             <template #item="{item}">
                 <EmailRegisterTile v-if="item.key === 'email'" @submit-success="tabIndex = 1"/>
-                <OtpVerificationTile v-else-if="item.key === 'otp'" />
+                <OtpVerificationTile v-else-if="item.key === 'otp'" @submit-success="tabIndex = 2"/>
+                <CreatePasswordTile v-else-if="item.key === 'password'" />
             </template>
         </UTabs>
     </Scaffold>
