43c93fe4deb99f989161a1c3fa39b5444448d05d
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-09 11:27:02 +0530

    added prop `isError` to look for error and change ui on if `isError = true`

--- components/account/utils/EnrichInput.vue
+++ components/account/utils/EnrichInput.vue
@@ -17,6 +17,12 @@ const props = defineProps({
         required: false,
         default: 'text'
     },
+
+    isError: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
 })
 
 const inputRef = ref<HTMLInputElement | null>(null);
@@ -39,7 +45,7 @@ const updateValue = (event: Event) => {
 <template>
 
     <div class="px-6 border border-gray-400/20 dark:border-gray-100/20 font-adaptive flex flex-col justify-start rounded-sm lg:col-span-2"
-         :class="`${disabled ? 'bg-gray-100 dark:bg-gray-700 py-3' : 'bg-white dark:bg-gray-800 py-1'}`"
+         :class="`${disabled ? 'bg-gray-100 dark:bg-gray-700 py-3' : 'bg-white dark:bg-gray-800 py-1'} ${isError ? 'border-red-400' : ''}`"
          @click="focusInput"
     >
         <span class="text-xs text-orange-400 dark:text-orange-500" :hidden="disabled">{{ label }}</span>
