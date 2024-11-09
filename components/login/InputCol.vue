8d28aef46b7b7ece9e1fcd8c5e736d816f53d161
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-10 01:05:13 +0530

    added advance error and success handling

--- components/login/InputCol.vue
+++ components/login/InputCol.vue
@@ -7,7 +7,16 @@ const props = defineProps({
         required: true
     },
     message: String,
-    error: String as PropType<InputState>
+    isError: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
+    isSuccess: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
 });
 
 
@@ -20,19 +29,8 @@ const updateValue = (event: Event) => {
 
 const STYLE_CONFIG = {
     'default': 'focus:ring-orange-300 dark:focus:ring-orange-500',
-    success: 'focus:ring-emerald-300 dark:focus:ring-emerald-500',
-    error: 'focus:ring-red-300 dark:focus:ring-red-500'
-}
-
-const processState = () => {
-    switch (props.error) {
-        case "DEFAULT" :
-            return STYLE_CONFIG.default;
-        case "SUCCESS":
-            return STYLE_CONFIG.success;
-        case "ERROR":
-            return STYLE_CONFIG.error;
-    }
+    success: 'ring-4 ring-emerald-300 dark:ring-emerald-500',
+    error: 'ring-4 ring-red-300 dark:focus:ring-red-500'
 }
 
 </script>
@@ -41,8 +39,8 @@ const processState = () => {
     <input
         :type="type"
         :placeholder="placeholder"
-        class="text-lg md:text-xl dark:bg-slate-600 border border-gray-200 dark:border-gray-800 focus:outline-none focus:ring-4 py-2 px-3 w-full rounded-md"
-        :class="`${processState()}`"
+        class="text-lg text-gray-600 dark:text-gray-300 dark:bg-slate-600 border border-gray-200 dark:border-gray-800 focus:outline-none focus:ring-4 py-2 px-3 w-full rounded-md"
+        :class="`${isError ? STYLE_CONFIG.error : STYLE_CONFIG.default} ${isSuccess ? STYLE_CONFIG.success : STYLE_CONFIG.default}`"
         @input="updateValue"
     >
 </template>
