14e90cc7fe8e4ec75f5886854b0cc4cf0d8c2305
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-15 18:19:23 +0530

    added method to add and get element from `OrderStore`

--- stores/order.store.ts
+++ stores/order.store.ts
@@ -6,6 +6,17 @@ export const useOrderStore = defineStore('orders', () => {
 
     const orders = ref<OrderModal[]>([])
 
+    const existsById = (id: number) => {
+        return orders.value.some(order => order.id === id);
+    }
 
-    return {orders}
+    const findById = (id: number): OrderModal | undefined => {
+        return orders.value.find(order => order.id === id);
+    }
+
+    const addToStore = (order: OrderModal) => {
+        if(!existsById(order.id)) orders.value.push(order);
+    }
+
+    return {orders, findById, existsById, addToStore}
 })
