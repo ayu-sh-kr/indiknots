852ede921bb2c8917b8957b155f7162e52d728e7
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-17 19:37:22 +0530

    change prop type to address for more control and less verbosity

--- components/account/order/view/OrderShipmentDescription.vue
+++ components/account/order/view/OrderShipmentDescription.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import type {OrderModal} from "~/modals/order.modal";
 import {getLocalAddress} from "~/modals/address.modal";
 
 const props = defineProps({
-    order: {
-        type: Object as PropType<OrderModal>,
+    address: {
+        type: Object as PropType<Address>,
         required: true
     }
 });
@@ -14,11 +13,11 @@ const props = defineProps({
     <div class="rounded-xl border-adaptive background-adaptive px-3 py-2 shadow-xl text-gray-700 dark:text-gray-300">
         <h4 class="text-lg md:text-xl border-b py-1.5">Shipment Details</h4>
 
-        <p class="font-semibold py-1.5 mt-2">{{order.address.name}}</p>
-        <p class="text-wrap text-sm text-gray-600 dark:text-gray-300">{{getLocalAddress(order.address)}}</p>
+        <p class="font-semibold py-1.5 mt-2">{{ address.name }}</p>
+        <p class="text-wrap text-sm text-gray-600 dark:text-gray-300">{{ getLocalAddress(address) }}</p>
 
         <p class="font-semibold py-1.5">Phone Number</p>
-        <p class="text-sm">{{order.address.phone.number}}</p>
+        <p class="text-sm">{{ address.phone.number }}</p>
     </div>
 </template>
 
