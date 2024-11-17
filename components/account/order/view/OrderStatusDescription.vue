dc6e2ef446c5e3325a9609e4a18f6ce18f7a4e7e
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-11-17 19:37:37 +0530

    component to render order status

---
+++ components/account/order/view/OrderStatusDescription.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+    status: {
+        type: Object as PropType<OrderStatus>,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <div class="w-full grid grid-cols-3 gap-2 border-l-2 border-orange-400 dark:border-orange-500">
+        <div>
+            <UBadge :label="status.value" color="blue" variant="soft"/>
+        </div>
+        <p class="text-sm max-md:text-xs text-gray-800 dark:text-gray-200">{{status.date.toDateString()}}</p>
+        <p class="text-sm max-md:text-xs text-gray-800 dark:text-gray-200">{{status.message}}</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
