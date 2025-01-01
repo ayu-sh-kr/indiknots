783d3c5004e62a560c4d8fbbbde43ab75aca421d
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-01-02 00:45:22 +0530

    page for verifying otp against email with backend

---
+++ pages/login/otp.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import LoginOTP from "~/components/login/LoginOTP.vue";
+
+definePageMeta({
+    layout: "login",
+});
+
+useHead({
+    title: 'Indiknots | Login'
+})
+</script>
+
+<template>
+    <LoginOTP/>
+</template>
+
+<style scoped>
+
+</style>
