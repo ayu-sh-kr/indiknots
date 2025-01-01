97fc759986f16a36dff723da3bd17fedb7cb1b42
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-01-02 00:43:43 +0530

    change definition due nuxt version upgrade

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: true},
-    css: ['~/assets/css/main.css'],
-    modules: [
-        "@nuxt/ui", "@vueuse/motion/nuxt", "nuxt-svgo", "@pinia/nuxt",
-        '@pinia-plugin-persistedstate/nuxt'
-    ],
-    components: true,
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
-    },
+  devtools: {enabled: true},
+  css: ['~/assets/css/main.css'],
+
+  modules: [
+      "@nuxt/ui", "@vueuse/motion/nuxt", "nuxt-svgo", "@pinia/nuxt",
+      '@pinia-plugin-persistedstate/nuxt'
+  ],
+
+  components: true,
+
+  postcss: {
+      plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+      },
+  },
+
+  compatibilityDate: "2025-01-01",
 })
