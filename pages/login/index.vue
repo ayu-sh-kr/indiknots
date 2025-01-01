6645b1913095f884ac5dd017f01279c35fa24b70
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-01-02 00:45:07 +0530

    page for processing email and generating otp

---
+++ pages/login/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import LoginEmail from "~/components/login/LoginEmail.vue";
+
+definePageMeta({
+    layout: "login",
+});
+
+useHead({
+    title: 'Indiknots | Login'
+})
+
+</script>
+
+<template>
+    <LoginEmail />
+</template>
+
+<style scoped>
+
+</style>
