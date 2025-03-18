f5a26d08a27eef6b74e6de7e9684c0c1412dfb0a
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-03-18 07:53:28 +0530

    feat: add EnrichedProductDTO class for managing enriched product data

---
+++ domains/product/EnrichedProductDTO.ts
@@ -0,0 +1,37 @@
+import {ProductDTO} from "~/domains/product/ProductDTO";
+import {ProductVariantDTO} from "~/domains/variant/ProductVariantDTO";
+import {ProductModal} from "~/domains/product/product.modal";
+
+
+export class EnrichedProductDTO implements ModalDTO<ProductModal>{
+    product: ProductDTO;
+    variants: ProductVariantDTO[];
+
+    constructor(product: ProductDTO, variants: ProductVariantDTO[]) {
+        this.product = product;
+        this.variants = variants;
+    }
+
+    toEntity(): ProductModal {
+        const productDTO = this.product;
+
+        return ProductModal.builder()
+            .id(productDTO.id.toString())
+            .name(productDTO.name)
+            .description(productDTO.description)
+            .category(productDTO.category)
+            .shape(productDTO.shape)
+            .material(productDTO.material)
+            .technique(productDTO.technique)
+            .animalFriendly(productDTO.isAnimalFriendly)
+            .variants(this.variants.map(variant => variant.toEntity()))
+            .build();
+    }
+
+    static convert(data: any): EnrichedProductDTO {
+        return new EnrichedProductDTO(
+            ProductDTO.convert(data.product),
+            data.variants.map((variant: any) => ProductVariantDTO.convert(variant))
+        );
+    }
+}
