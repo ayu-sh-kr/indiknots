be4301db07d71ffe903a79ab44f424d94006135a
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-03-18 07:51:22 +0530

    feat: add StockDTO class for managing product stock data

---
+++ domains/stock/StockDTO.ts
@@ -0,0 +1,33 @@
+import {ProductStockModal} from "~/domains/stock/product-stock.modal";
+
+export class StockDTO implements ModalDTO<ProductStockModal>{
+    id: number;
+    quantity: number;
+    status: ProductStockStatus;
+
+    constructor(
+        id: number,
+        quantity: number,
+        status: ProductStockStatus,
+    ) {
+        this.id = id;
+        this.quantity = quantity;
+        this.status = status;
+    }
+
+    toEntity(): ProductStockModal {
+        return ProductStockModal.builder()
+            .id(this.id)
+            .quantity(this.quantity)
+            .status(this.status)
+            .build();
+    }
+
+    static convert(data: any): StockDTO {
+        return new StockDTO(
+            data.id,
+            data.quantity,
+            data.status
+        );
+    }
+}
