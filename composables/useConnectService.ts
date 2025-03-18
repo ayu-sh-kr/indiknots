3f3b50d1cfed1673806abe417e10aca2da20c533
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-03-19 03:54:41 +0530

    fix: update ConnectService import path in useConnectService.ts

--- composables/useConnectService.ts
+++ composables/useConnectService.ts
@@ -1,4 +1,4 @@
-import {ConnectService} from "~/domains/connect/connect.modal";
+import {ConnectService} from "~/domains/connect/connect.service";
 
 export const useConnectService = () => {
   return new ConnectService('http://localhost:8080', useToastService())
