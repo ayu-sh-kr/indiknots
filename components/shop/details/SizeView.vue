151e912eb25bed9c27ffa0e490eab3fc730f0298
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-03-18 18:37:23 +0530

    feat: update SizeView.vue to use ProductVariantModal and ProductSizeModal for size processing

--- components/shop/details/SizeView.vue
+++ components/shop/details/SizeView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 
-const props = defineProps({
-    size: {
-        type: Object as PropType<ProductSize>,
+import type {ProductVariantModal} from "~/domains/variant/product-variant.modal";
+import type {ProductSizeModal} from "~/domains/size/product-size.modal";
+
+defineProps({
+    variant: {
+        type: Object as PropType<ProductVariantModal>,
         required: true
     },
 
@@ -12,13 +15,13 @@ const props = defineProps({
     }
 });
 
-const processProductLength = (size: ProductSize) => {
+const processProductLength = (size: ProductSizeModal) => {
 
     if(size.width === 0) {
-        return `Rounded ${size.length} ${size.unit}`
+        return `Rounded ${size.length} ${size.sizeUnit}`
     }
 
-    return `${size.length} x ${size.width} ${size.unit}`
+    return `${size.length} x ${size.width} ${size.sizeUnit}`
 }
 
 </script>
@@ -27,7 +30,7 @@ const processProductLength = (size: ProductSize) => {
     <div  class="px-2 py-1 text-center cursor-pointer"
           :class="`${selected ? 'bg-orange-400 dark:bg-orange-500 text-gray-50': 'border border-gray-600 dark:border-gray-200 text-gray-800  dark:text-gray-200'}`"
     >
-        {{processProductLength(size)}}
+        {{processProductLength(variant.size)}}
     </div>
 </template>
 
