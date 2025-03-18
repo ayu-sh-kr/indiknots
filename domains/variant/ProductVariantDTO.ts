cda6594d9bd3e0fd637e541e870a1fa1f6f1201b
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-03-18 07:51:41 +0530

    feat: add ProductVariantDTO class for managing product variant data

---
+++ domains/variant/ProductVariantDTO.ts
@@ -0,0 +1,53 @@
+import {StockDTO} from "~/domains/stock/StockDTO";
+import {SizeDTO} from "~/domains/size/SizeDTO";
+import {PriceDTO} from "~/domains/price/PriceDTO";
+import {ImageDTO} from "~/domains/image/ImageDTO";
+import {ProductVariantModal} from "~/domains/variant/product-variant.modal";
+
+export class ProductVariantDTO implements ModalDTO<ProductVariantModal>{
+
+    id: number;
+    productId: number;
+    size: SizeDTO;
+    stock: StockDTO;
+    price: PriceDTO;
+    images: ImageDTO[];
+
+    constructor(
+        id: number,
+        productId: number,
+        size: SizeDTO,
+        stock: StockDTO,
+        price: PriceDTO,
+        images: ImageDTO[]
+    ) {
+        this.id = id;
+        this.productId = productId;
+        this.size = size;
+        this.stock = stock;
+        this.price = price;
+        this.images = images;
+    }
+
+    toEntity(): ProductVariantModal {
+        return ProductVariantModal.builder()
+            .id(this.id)
+            .productId(this.productId)
+            .size(this.size.toEntity())
+            .stock(this.stock.toEntity())
+            .price(this.price.toEntity())
+            .images(this.images.map(image => image.toEntity()))
+            .build();
+    }
+
+    static convert(data: any): ProductVariantDTO {
+        return new ProductVariantDTO(
+            data.id,
+            data.productId,
+            SizeDTO.convert(data.size),
+            StockDTO.convert(data.stock),
+            PriceDTO.convert(data.price),
+            data.images.map((image: any) => ImageDTO.convert(image))
+        );
+    }
+}
