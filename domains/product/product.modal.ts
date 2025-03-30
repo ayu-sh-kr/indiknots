728967a4983c636be1be5bf1a1859c488f4d7838
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-03-30 10:33:34 +0530

    feat: remove unused methods and imports from ProductModal

--- domains/product/product.modal.ts
+++ domains/product/product.modal.ts
@@ -1,7 +1,4 @@
-import type {CartStore} from "~/stores/cart.store";
-import {CartModal} from "~/domains/cart/cart.modal";
 import {ProductVariantModal} from "~/domains/variant/product-variant.modal";
-import {CartUtils} from "~/domains/cart/cart.utils";
 
 class ProductModal implements Product {
 
@@ -61,42 +58,6 @@ class ProductModal implements Product {
         return processUnderscoreText(this.category)
     }
 
-    /**
-     * Retrieves the size text for a given size.
-     * @param size - The size of the product.
-     * @returns The size text.
-     */
-    getSizeText(size: ProductSize) {
-        return `${size.length} x ${size.width} ${size.unit}`
-    }
-
-
-    /**
-     * Calculates and returns the discounted price for a given price.
-     * @param price - The original price details.
-     * @returns The discounted price.
-     */
-    getDiscountedPrice(price: ProductPrice) {
-        const originalPrice = price.value;
-        const discount = price.sale_percentage;
-        const discountPrice = originalPrice - originalPrice * (discount / 100);
-        return Math.round(discountPrice * 100) / 100;
-    }
-
-
-    /**
-     * Retrieves the size options available for the product.
-     * @returns An array of size options.
-     */
-    getSizeOptions(): ProductSizeOption[] {
-        return this.prices.map(price => {
-            return {
-                label: this.getSizeText(price.size),
-                value: price.size
-            } as ProductSizeOption
-        })
-    }
-
     static builder() {
         return new ProductBuilder();
     }
@@ -126,18 +87,8 @@ class ProductBuilder {
         return this;
     }
 
-    color(color: string): ProductBuilder {
-        this.product.color = color;
-        return this;
-    }
-
     description(description: string): ProductBuilder {
         this.product.description = description;
-        return this;
-    }
-
-    img(img: ProductImage[]): ProductBuilder {
-        this.product.img = img;
         return this;
     }
 
@@ -153,11 +104,6 @@ class ProductBuilder {
 
     price(price: ProductPrice[]): ProductBuilder {
         this.product.prices = price;
-        return this;
-    }
-
-    sale(sale: boolean): ProductBuilder {
-        this.product.sale = sale;
         return this;
     }
 
@@ -204,28 +150,7 @@ const processUnderscoreText = (text: string) => {
 }
 
 
-/**
- * Handles the action of adding a product to the cart.
- *
- * This function creates a new `CartModal` instance using the `CartModalBuilder` class,
- * populates it with the provided product details, and then calls the `cartAction2Handler`
- * function to process the cart action.
- *
- * @param product - The `ProductModal` instance representing the product to be added to the cart.
- * @param cartStore - The `CartStore` instance representing the cart store where the product will be added.
- * @returns The result of the `cartAction2Handler` function, which processes the cart action.
- */
-const cartActionHandler = (product: ProductModal, cartStore: CartStore) => {
-    const cartModal = CartModal.builder()
-        .product(product)
-        .productId(product.id)
-        .quantity(1)
-        .color(product.color)
-        .variant(product.variants[0])
-        .build();
 
-    return CartUtils.cartAction2Handler(cartModal, cartStore)
-}
 
 const getSizeText = (size: ProductSize) => {
     return `${size.length} x ${size.width} ${size.unit}`
@@ -239,4 +164,4 @@ const getDiscountedPrice = (price: ProductPrice) => {
 }
 
 
-export {ProductModal, ProductBuilder, processUnderscoreText, cartActionHandler, getSizeText, getDiscountedPrice}
+export {ProductModal, ProductBuilder, processUnderscoreText, getSizeText, getDiscountedPrice}
