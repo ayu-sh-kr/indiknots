1fe6a74f1737d1ff3fa284eb7bb24a200cf1ff49
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-03-30 10:29:25 +0530

    feat: update CartUtils to use CartItemModal and integrate CartService for cart operations

--- domains/cart/cart.utils.ts
+++ domains/cart/cart.utils.ts
@@ -1,5 +1,6 @@
-import {CartModal} from "~/domains/cart/cart.modal";
+import {CartItemModal} from "~/domains/cart/cart-item.modal";
 import {ProductUtils} from "~/domains/product/product.utils";
+import {CartService} from "~/domains/cart/cart.service";
 
 
 export class CartUtils {
@@ -13,13 +14,32 @@ export class CartUtils {
    * @param cartStore - The `CartStore` instance managing the cart state.
    * @returns `true` if the product was added to the cart, `false` if the product was removed from the cart.
    */
-  static cartAction2Handler = (cartModal: CartModal, cartStore: CartStore) => {
-    if(!cartStore.isProductExist(cartModal.productId) && cartModal.variant.stock.status === "AVAILABLE") {
+  static cartAction2Handler = (cartModal: CartItemModal, cartStore: CartStore) => {
+    // Check if user is logged in
+    if(!cartStore.isProductExist(cartModal.productId) && ProductUtils.isStockAvailable(cartModal.variant)) {
       cartStore.addToCart(cartModal)
       return true
     } else {
-      cartStore.removeFromCart(cartModal.productId)
+      cartStore.removeFromCart(cartModal.id)
       return false
+    }
+  }
+
+  /**
+   * Handles the action of adding or removing a product from the cart.
+   * If the product does not exist in the cart and is available in stock, it adds the product to the cart.
+   * If the product already exists in the cart, it removes the product from the cart.
+   *
+   * @param cartModal - The `CartModal` instance representing the product to be added or removed.
+   * @param cartService - The `CartService` instance managing the cart operations.
+   */
+  static cartAction3Handler = async (cartModal: CartItemModal, cartService: CartService) => {
+    if(!(await cartService.existsInCart(cartModal.productId, cartModal.variant.id)) && ProductUtils.isStockAvailable(cartModal.variant)) {
+      await cartService.addToCart(cartModal)
+      return true;
+    } else {
+      await cartService.removeFromCart(cartModal);
+      return false;
     }
   }
 
@@ -33,7 +53,7 @@ export class CartUtils {
    * @param cartModals - An array of `CartModal` instances representing the items in the cart.
    * @returns The total price of all items in the cart.
    */
-  static getTotalPrice = (cartModals: CartModal[]) => {
+  static getTotalPrice = (cartModals: CartItemModal[]) => {
     let price = 0;
     cartModals.forEach(modal => {
       price += modal.variant.price.value * modal.quantity
@@ -53,7 +73,7 @@ export class CartUtils {
    * @param cartModals - An array of `CartModal` instances representing the items in the cart.
    * @returns The total discounted price of all items in the cart.
    */
-  static getDiscountedPrice = (cartModals: CartModal[]) => {
+  static getDiscountedPrice = (cartModals: CartItemModal[]) => {
     let discounted = 0;
     cartModals.forEach(modal => {
       discounted += ProductUtils.getDiscountedPrice(modal.variant) * modal.quantity
